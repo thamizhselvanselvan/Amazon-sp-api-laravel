779df32ce9d3216f4d946c0958116c9e95643fe5
amitpms <nu.palmate@gmail.com>
2022-01-14 17:28:35 +0530

    add columns in products

---
+++ database/migrations/2022_01_14_111035_alter_products_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products',function (Blueprint $table){
+
+            $table->string('brand');
+            $table->string('product_group');
+            $table->string('product_type_name');
+            $table->string('publisher');
+            $table->string('manufacturer');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products',function(Blueprint $table){
+
+            $table->dropColumn([
+                'brand',
+                'product_group',
+                'product_type_name',
+                'publisher',
+                'manufacturer'
+            ]);
+        });
+    }
+}
