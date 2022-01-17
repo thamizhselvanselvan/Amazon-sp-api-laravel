48744168727c0b108fbdc5472b21eebd85ed9c5d
amitpms <nu.palmate@gmail.com>
2022-01-17 19:34:44 +0530

    Getting data from catlogapi and saving into database

--- app/Http/Controllers/SaveAsinController.php
+++ app/Http/Controllers/SaveAsinController.php
@@ -5,9 +5,12 @@ namespace App\Http\Controllers;
 use App\Services\SpApi;
 use App\Models\Products;
 use App\Models\SaveAsin;
+use ClouSale\AmazonSellingPartnerAPI\Models\FbaSmallAndLight\MarketplaceId;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use ClouSale\AmazonSellingPartnerAPI\Models\ProductPricing\Product;
+use Illuminate\Support\Arr;
+use PhpParser\JsonDecoder;
 use \RedBeanPHP\R as R;
 
 class SaveAsinController extends Controller
@@ -32,96 +35,156 @@ class SaveAsinController extends Controller
     public function show(Request $request)
     {
         $marketplace = 'A21TJRUUN4KGV';
-        echo "<PRE>";
 
-        if (file_exists('B073SBZ8YH.txt')) {
-            echo 'reading from file';
-            $response = json_decode(file_get_contents('B073SBZ8YH.txt'));
-
-            print_r($response);
-
-            $book = R::dispense("book");
-            $book->author = "Santa Claus";
-            $book->$$title = "Secrets of Christmas";
-            $book->name = "gopal";
-            $book->age = "22";
-            $book->mobile = "1234";
-            R::store($book);
-
-
-
-            //print_r($response->AttributeSets->Brand);
-        } else {
-            echo 'reading from amazon';
-            $sp_api = new SpApi;
-            $response = $sp_api->catalogApitest($marketplace, 'B073SBZ8YH');
-            file_put_contents('B073SBZ8YH.txt', json_encode($response));
-        }
-
-        //print_r($response);
-        exit;
-
-
-        R::setup('mysql:host=localhost:8001;dbname=sp_api', 'root', 'root');
-        R::debug(TRUE);
-
-        //$book  = R::find( 'book', ' id = 2 ');
-        //$book = R::load('book', 3);
-
-        $book = R::dispense("book");
-        $book->author = "Santa Claus";
-        $book->title = "Secrets of Christmas";
-        $book->name = "gopal";
-        $book->age = "22";
-        $book->mobile = "1234";
-        R::store($book);
-
-        exit;
-
-
+        R::setup('mysql:host=localhost;dbname=spapi', 'root', 'root');
 
         $titles = preg_split("/\r\n| |''|,/", $request->asinText);
+
+        
+        R::exec('TRUNCATE `product`');
+
         $newData = [];
 
         foreach ($titles as $title) {
+
+            $product = R::dispense("product");
+
             if (!empty(trim($title))) {
-                $newData[] = trim($title);
+
+                $title = trim($title);
+             
+                if (file_exists($title.'.txt')) {
+
+                    echo 'reading from file <BR>';
+                   
+                    $response = json_decode(file_get_contents($title.'.txt'));
+
+                    foreach ($response->AttributeSets[0] as $key => $value) {
+
+                        $data = "";
+
+                        $key = lcfirst($key);
+
+                        print_r("Key:" . $key);
+                        echo "<BR>";
+                        echo "Value: ";
+
+                        if (is_array($value)) {
+
+                            $data = json_encode($value);
+                        } 
+                        else if (is_object($value)) {
+
+                            $temp = (array) $value;
+                            $data = json_encode($temp);
+                        } 
+                        else {
+
+                            $data =  $value;
+                        }
+                        echo $data;
+
+                        $product->{$key} = $data;
+
+                        echo "<HR>";
+                    }
+                    R::store($product);
+             
+
+            } 
+            else {
+
+            echo 'reading from amazon';
+           
+
+            $sp_api = new SpApi;
+            $response = $sp_api->catalogApitest($marketplace, $title);
+
+
+            file_put_contents($title.'.txt', Json_encode(Json_decode($response)));
+
+            
+            foreach (Json_decode($response)->AttributeSets[0] as $key => $value) {
+
+                $data = "";
+
+                $key = lcfirst($key);
+
+
+                print_r("Key:" . $key);
+                echo "<BR>";
+                echo "Value: ";
+
+                if (is_array($value)) {
+
+                    $data = json_encode($value);
+                } 
+                else if (is_object($value)) {
+
+                    $temp = (array) $value;
+                    $data = json_encode($temp);
+                } 
+                else {
+
+                    $data =  $value;
+                }
+                echo $data;
+
+                $product->{$key} = $data;
+
+                echo "<HR>";
+            }
+
+                R::store($product);
+
+                sleep(2);
+
             }
         }
+    }
 
-        echo "<PRE>";
+        // $titles = preg_split("/\r\n| |''|,/", $request->asinText);
+        // $newData = [];
 
-        // return $newData;
-        $count = 0;
-        $title = [];
-        $sp_api = new SpApi;
+        // foreach ($titles as $title) {
+        //     if (!empty(trim($title))) {
+        //         $newData[] = trim($title);
+        //     }
+        // }
 
-        print_r($newData);
+        // echo "<PRE>";
 
-        foreach ($newData as $data) {
+        // // return $newData;
+        // $count = 0;
+        // $title = [];
+        // $sp_api = new SpApi;
 
-            $count++;
-            if ($count > 5) {
-                break;
-            }
+        // print_r($newData);
 
-            $response = $sp_api->catalogApitest($marketplace, $data);
-            $product = $response->getAttributeSets();
-            $product = $product[0];
-            /*
-            print_r($product->getTitle());
-            print_r($product->getListPrice());
-            print_r($product->getPackageDimensions()->getHeight()->getValue());
-            print_r($product->getPackageDimensions()->getHeight()->getunits());
-            */
-            print_r($product);
+        // foreach ($newData as $data) {
 
-            exit;
-            sleep(2);
+        //     $count++;
+        //     if ($count > 5) {
+        //         break;
+        //     }
 
-            //DB::table('products')->insert(['ASIN' => $data, 'Title' => $title]);
+        //     $response = $sp_api->catalogApitest($marketplace, $data);
+        //     $product = $response->getAttributeSets();
+        //     $product = $product[0];
+        //     /*
+        //     print_r($product->getTitle());
+        //     print_r($product->getListPrice());
+        //     print_r($product->getPackageDimensions()->getHeight()->getValue());
+        //     print_r($product->getPackageDimensions()->getHeight()->getunits());
+        //     */
+        //     print_r($product);
 
-            // return $sp_api->catalogApitest($marketplace, $data);
-        }
+        //     exit;
+        //     sleep(2);
+
+        //     //DB::table('products')->insert(['ASIN' => $data, 'Title' => $title]);
+
+        //     // return $sp_api->catalogApitest($marketplace, $data);
+        // }
     }
 }
--- config/app.php
+++ config/app.php
@@ -134,6 +134,17 @@ return [
     |
     */
 
+
+    /*
+    / Database configuration 
+    */
+    'connection' => env('DB_CONNECTION'),
+    'host' => env('DB_HOST'),
+    'port' => env('DB_PORT'),
+    'database' => env('DB_DATABASE'),
+    'username' => env('DB_USERNAME'),
+    'password' => env('DB_PASSWORD'),
+
     'providers' => [
 
         /*
