1e8dc1e10a01f16c372a93addecf0c1c4bd9a91c
Robin Singh <robin.flyhigh@gmail.com>
2022-02-16 15:56:16 +0530

    pk in prod

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        DB::statement('SET SESSION sql_require_primary_key=0');
+        if (app()->environment() === 'production') {
+            DB::statement('SET SESSION sql_require_primary_key=0');
+        }
     }
 }
