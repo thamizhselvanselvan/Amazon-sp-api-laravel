1696a6e7e4b158fc14331df2c551f23e5dc2c237
amitpms <nu.palmate@gmail.com>
2022-02-25 18:09:34 +0530

    pms:catalog-import command implemention

--- app/Console/Commands/ProductCatalogImport.php
+++ app/Console/Commands/ProductCatalogImport.php
@@ -2,10 +2,15 @@
 
 namespace App\Console\Commands;
 
+use Exception;
+use RedBeanPHP\R;
+use App\Models\asinMaster;
 use Illuminate\Console\Command;
+use App\Services\Config\ConfigTrait;
+use SellingPartnerApi\Api\CatalogItemsV0Api;
 
 class ProductCatalogImport extends Command
-{
+{    use ConfigTrait;
     /**
      * The name and signature of the console command.
      *
@@ -37,6 +42,61 @@ class ProductCatalogImport extends Command
      */
     public function handle()
     {
-        echo 'success';
+        $datas = asinMaster::with(['aws'])->offset(101)->limit(10)->get();
+        $connection = config('app.connection');
+        $host = config('app.host');
+        $dbname = config('app.database');
+        $username = config('app.username');
+        $password = config('app.password');
+    
+        R::setup('mysql: host='.$host.'; dbname='.$dbname, $username, $password); 
+        R::exec('TRUNCATE `productcatalogs`'); 
+    
+        foreach($datas as $data){
+            $asin = $data['asin'];
+            $country_code = $data['destination_1'];
+            $auth_code = $data['aws']['auth_code'];
+            $aws_key = $data['aws']['id'];
+            $marketplace_id = $this->marketplace_id($country_code);
+    
+            $config = $this->config($aws_key, $country_code, $auth_code);
+    
+            $apiInstance = new CatalogItemsV0Api($config);
+            $marketplace_id = $this->marketplace_id($country_code);
+        
+            try {
+                $result = $apiInstance->getCatalogItem($marketplace_id, $asin);
+                
+                $result = json_decode(json_encode($result));
+                
+                $result = (array)($result->payload->AttributeSets[0]);
+                
+                $productcatalogs = R::dispense('productcatalogs');
+            
+                $value = [];
+                $productcatalogs->asin = $asin;
+                
+                foreach ($result as $key => $data){
+                    $key = lcfirst($key);
+                    if(is_object($data)){
+            
+                        $productcatalogs->{$key} = json_encode($data);
+                    }
+                    else
+                    {
+                        $productcatalogs->{$key} = json_encode($data);
+                        // $value [][$key] = ($data);
+                    }
+    
+                }
+                R::store($productcatalogs);
+                
+                
+            } catch (Exception $e) {
+                echo 'Exception when calling CatalogItemsV0Api->getCatalogItem: ', $e->getMessage(), PHP_EOL;
+            }
+            
+            
+        }
     }
 }
--- app/Http/Controllers/product/productController.php
+++ app/Http/Controllers/product/productController.php
@@ -23,75 +23,13 @@ use SellingPartnerApi\Api\CatalogItemsV0Api;
 
 class productController extends Controller
 {
-    use ConfigTrait;
+   
     public function index(){
 
         return view('product.index');
     }
 
 public function fetchFromAmazon(){
-
-    $datas = asinMaster::with(['aws'])->limit(100)->get();
-    $connection = config('app.connection');
-    $host = config('app.host');
-    $dbname = config('app.database');
-    $username = config('app.username');
-    $password = config('app.password');
-
-    R::setup('mysql: host='.$host.'; dbname='.$dbname, $username, $password); 
-    R::exec('TRUNCATE `productcatalogs`'); 
-
-    foreach($datas as $data){
-        $asin = $data['asin'];
-        $country_code = $data['destination_1'];
-        $auth_code = $data['aws']['auth_code'];
-        $aws_key = $data['aws']['id'];
-        $marketplace_id = $this->marketplace_id($country_code);
-
-        $config = $this->config($aws_key, $country_code, $auth_code);
-
-        $apiInstance = new CatalogItemsV0Api($config);
-        $marketplace_id = $this->marketplace_id($country_code);
-    
-        try {
-            $result = $apiInstance->getCatalogItem($marketplace_id, $asin);
-            
-            $result = json_decode(json_encode($result));
-            
-            $result = (array)($result->payload->AttributeSets[0]);
-            
-            $productcatalogs = R::dispense('productcatalogs');
-        
-            $value = [];
-            $productcatalogs->asin = $asin;
-            
-            foreach ($result as $key => $data){
-                $key = lcfirst($key);
-                if(is_object($data)){
-        
-                    $productcatalogs->{$key} = json_encode($data);
-                }
-                else
-                {
-                    $productcatalogs->{$key} = json_encode($data);
-                    // $value [][$key] = ($data);
-                }
-
-            }
-            R::store($productcatalogs);
-            
-            
-        } catch (Exception $e) {
-            echo 'Exception when calling CatalogItemsV0Api->getCatalogItem: ', $e->getMessage(), PHP_EOL;
-        }
-        
-        
-    }
-    /*
-    *
-    * command start
-    *
-    */
 
     if (App::environment(['Production', 'Staging', 'production', 'staging'])) {
 
@@ -108,9 +46,6 @@ public function fetchFromAmazon(){
         Artisan::call('pms:catalog-import');
         
     }
-    // command end
-
-
-     
+       
     }
 }
