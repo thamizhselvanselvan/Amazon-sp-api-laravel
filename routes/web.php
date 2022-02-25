23756215cca22f43e2f52a56c33c358554a1c778
amitpms <nu.palmate@gmail.com>
2022-02-25 20:10:44 +0530

    final final

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Storage;
+use RedBeanPHP\R;
 
 /*
 |--------------------------------------------------------------------------
