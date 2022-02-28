bcb88a41d3fbc4ef4a558fb96b17e71dca13bf73
amitpms <nu.palmate@gmail.com>
2022-02-28 11:56:11 +0530

    Add try catch and finally for testing

--- app/Services/SP_API/CatalogImport.php
+++ app/Services/SP_API/CatalogImport.php
@@ -25,60 +25,68 @@ class CatalogImport
         $port = config('app.port');
         $username = config('app.username');
         $password = config('app.password');
+        
+        $datas = asinMaster::with(['aws'])->limit(10)->get();
 
         Log::warning('host->' . $host . ',port->'. $port .',dbname->' . $dbname . ',username->' . $username . 'password->' . $password);
-        $setup = R::setup('mysql: host='.$host.';dbname='.$dbname.';port='.$port,$username,$password);
-            dd($setup);
-        Log::alert("working");
+        try
+        {
+            R::setup('mysql: host=' . $host . '; dbname=' . $dbname . ';port=' . $port, $username, $password);
 
-        $datas = asinMaster::with(['aws'])->limit(10)->get();
-        
-        foreach ($datas as $data) {
-            
-
-            $asin = $data['asin'];
-
-            $country_code = $data['destination_1'];
-            $auth_code = $data['aws']['auth_code'];
-            $aws_key = $data['aws']['id'];
-            $marketplace_id = $this->marketplace_id($country_code);
-
-            $config = $this->config($aws_key, $country_code, $auth_code);
-
-            $apiInstance = new CatalogItemsV0ApiProduct($config);
-            $marketplace_id = $this->marketplace_id($country_code);
-            Log::warning("try to get catalog data");
-            try {
-                $result = $apiInstance->getCatalogItem($marketplace_id, $asin);
-
-                $result = json_decode(json_encode($result));
-
-                $result = (array)($result->payload->AttributeSets[0]);
-
-                $productcatalogs = R::dispense('amazon');
-                Log::alert("amazon table created");
-                $value = [];
-                $productcatalogs->asin = $asin;
-                $productcatalogs->destination = $country_code;
-
-                foreach ($result as $key => $data) {
-                    $key = lcfirst($key);
-                    if (is_object($data)) {
-
-                        $productcatalogs->{$key} = json_encode($data);
-                    } else {
-                        $productcatalogs->{$key} = json_encode($data);
-                        // $value [][$key] = ($data);
-                    }
-                }
-
-                R::store($productcatalogs);
-                Log::alert('product catalog saved');
+            foreach ($datas as $data) {
                 
-            } catch (Exception $e) {
-                Log::notice($e->getMessage());
+                $asin = $data['asin'];
+    
+                $country_code = $data['destination_1'];
+                $auth_code = $data['aws']['auth_code'];
+                $aws_key = $data['aws']['id'];
+                $marketplace_id = $this->marketplace_id($country_code);
+    
+                $config = $this->config($aws_key, $country_code, $auth_code);
+    
+                $apiInstance = new CatalogItemsV0ApiProduct($config);
+                $marketplace_id = $this->marketplace_id($country_code);
+                Log::warning("try to get catalog data");
+                try {
+                    $result = $apiInstance->getCatalogItem($marketplace_id, $asin);
+    
+                    $result = json_decode(json_encode($result));
+    
+                    $result = (array)($result->payload->AttributeSets[0]);
+    
+                    $productcatalogs = R::dispense('amazon');
+                    Log::alert("amazon table created");
+                    $value = [];
+                    $productcatalogs->asin = $asin;
+                    $productcatalogs->destination = $country_code;
+    
+                    foreach ($result as $key => $data) {
+                        $key = lcfirst($key);
+                        if (is_object($data)) {
+    
+                            $productcatalogs->{$key} = json_encode($data);
+                        } else {
+                            $productcatalogs->{$key} = json_encode($data);
+                            // $value [][$key] = ($data);
+                        }
+                    }
+    
+                    R::store($productcatalogs);
+                    Log::alert('product catalog saved');
+                    
+                } catch (Exception $e) {
+                    Log::notice($e->getMessage());
+                }
+                
             }
-            
         }
+        catch(Exception $e){
+            Log::alert($e->getMessage());
+        }finally{
+
+            Log::alert("working");
+        }
+
+        
     }
 }
