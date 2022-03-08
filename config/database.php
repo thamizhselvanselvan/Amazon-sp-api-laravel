06314af8c2925ae9c18a744786b42dc6ad943d3a
Robin Singh <robin.flyhigh@gmail.com>
2022-03-08 16:31:53 +0530

    wip

--- config/database.php
+++ config/database.php
@@ -70,17 +70,34 @@ return [
             'strict' => false,
             'engine' => 'InnoDB ROW_FORMAT=DYNAMIC',
         ],
+
         'aws' => [
             'driver' => 'mysql',
             'host' => env('AWS_DB_HOST', '127.0.0.1'),
             'port' => env('AWS_DB_PORT', '3306'),
-            'database' => env('AWS_DB_DATABASE', 'acm'),
-            'username' => env('AWS_DB_USERNAME', 'root'),
-            'password' => env('AWS_DB_PASSWORD', 'root'),
+            'database' => env('AWS_DB_DATABASE', ''),
+            'username' => env('AWS_DB_USERNAME', ''),
+            'password' => env('AWS_DB_PASSWORD', ''),
             'unix_socket' => env('AWS_DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => env('AWS_DB_PREFIX',''),
+            'prefix' => env('AWS_DB_PREFIX', ''),
+            'prefix_indexes' => false,
+            'strict' => false,
+            'engine' => 'InnoDB ROW_FORMAT=DYNAMIC',
+        ],
+
+        'mssql' => [
+            'driver' => 'mssql',
+            'host' => env('MSSQL_DB_HOST', '127.0.0.1'),
+            'port' => env('MSSQL_DB_PORT', '3306'),
+            'database' => env('MSSQL_DB_DATABASE', ''),
+            'username' => env('MSSQL_DB_USERNAME', ''),
+            'password' => env('MSSQL_DB_PASSWORD', ''),
+            'unix_socket' => env('MSSQL_DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => env('MSSQL_DB_PREFIX', ''),
             'prefix_indexes' => false,
             'strict' => false,
             'engine' => 'InnoDB ROW_FORMAT=DYNAMIC',
@@ -118,7 +135,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
