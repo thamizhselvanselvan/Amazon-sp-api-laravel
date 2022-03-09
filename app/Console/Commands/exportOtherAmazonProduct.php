10d1c723b6f1f79f1782162b6462d4ee505fc81d
amitpms <nu.palmate@gmail.com>
2022-03-09 12:54:55 +0530

    record per csv and chunk value set to 1000000, 100000

--- app/Console/Commands/exportOtherAmazonProduct.php
+++ app/Console/Commands/exportOtherAmazonProduct.php
@@ -45,8 +45,8 @@ class exportOtherAmazonProduct extends Command
     {
         Log::warning("warning form exprot ");
         $file_path = "excel/downloads/otheramazon/otherProductDetails";
-        $record_per_csv = 900000 ;
-        $chunk = 50000;
+        $record_per_csv = 1000000 ;
+        $chunk = 100000;
         $this->check = $record_per_csv/$chunk;
         Log::warning($this->check);
         $header = ['hit', 'asin', 'sku', 'hs_code', 'gst', 'update_time', 'availability', 'price', 'list_price', 'price1', 'price_inr', 'list_price_inr', 'price_aed', 'list_price_aed', 'shipping_weight', 'image_t', 'id', 'title', 'image_p', 'image_d', 'category', 'all_category', 'description', 'height', 'length', 'width', 'weight', 'flipkart', 'amazon', 'upc', 'manufacturer	', 'latency', 'uae_latency', 'b2c_latency', 'ean', 'color', 'model', 'mpn', 'detail_page_url', 'creation_time', 'page'];
