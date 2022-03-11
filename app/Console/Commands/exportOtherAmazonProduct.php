1b429bbcc2a450a293d2d6405ffc3820d31028e4
amitpms <nu.palmate@gmail.com>
2022-03-11 19:42:00 +0530

    update chunk

--- app/Console/Commands/exportOtherAmazonProduct.php
+++ app/Console/Commands/exportOtherAmazonProduct.php
@@ -66,8 +66,8 @@ class exportOtherAmazonProduct extends Command
         }
 
         $exportFilePath = "excel/downloads/otheramazon/otherProductDetails";
-        $record_per_csv = 10000;
-        $chunk = 1000;
+        $record_per_csv = 1000000;
+        $chunk = 100000;
         
         $this->check = $record_per_csv / $chunk;
 
