0b67292a027199e8e405de61bc92431cb613d97b
amitpms <nu.palmate@gmail.com>
2022-03-21 17:57:05 +0530

    event tracking Status fixed

--- app/Http/Controllers/B2cship/TrackingStatusController.php
+++ app/Http/Controllers/B2cship/TrackingStatusController.php
@@ -9,19 +9,58 @@ use App\Http\Controllers\Controller;
 class TrackingStatusController extends Controller
 {
     public function trackingStatusDetails()
-    {
-        $PODtransEvents = DB::connection('mssql')->select("SELECT DISTINCT ISNULL(FPCode,'B2CShip')+ ' : ' + StatusDetails AS TrackingMsg FROM PODTrans");
+    {   $PODeventsArray =[];
+        $offset = 0;
 
+        $PODtransEvents = DB::connection('mssql')->select("SELECT DISTINCT ISNULL(FPCode,'B2CShip')+ ' : ' + StatusDetails AS TrackingMsg FROM PODTrans");
+        
+        // Making By default null for every code and description
         foreach ($PODtransEvents as $PODtransEvent) {
 
-            $PODeventsArray[] = $PODtransEvent->TrackingMsg;
+            $PODeventsArray[$offset]['TrackingMsg'] = $PODtransEvent->TrackingMsg;
+            $PODeventsArray[$offset]['TrackingMasterCode'] = NULL;
+            $PODeventsArray[$offset]['TrackingMasterEventDescription'] = NULL;
+            $PODeventsArray[$offset]['OurEventCode'] = NULL;
+            $PODeventsArray[$offset]['EventDescription'] = NULL;
+            $offset++;
         }
+
+       //our master event table and Tracking Msg
         $trackingEventsMapping = DB::connection('mssql')->select("SELECT TrackingMsg, TrackingMasterCode, OurEventCode, EventDescription FROM TrackingEventMapping");
-        
+
+        //Amazon master event table
         $trackingEventsMaster = DB::connection('mssql')->select("SELECT TrackingEventCode, EventCodeDescription FROM TrackingEventMaster");
-        
-        po($trackingEventsMaster);
-       
-    
+
+        $trackingEventsMasterArray = [];
+
+        //making associative array for amazon master tracking table according to code and Description
+        //eg:- [EVENT_101] => Shipment has been Booked
+
+        foreach ($trackingEventsMaster as $trackingEventMaster) {
+            $trackingEventsMasterArray[$trackingEventMaster->TrackingEventCode] = $trackingEventMaster->EventCodeDescription;
+        }
+
+        $offset = 0;
+
+        foreach ($PODeventsArray as $PODeventskey => $PODeventArray) {
+            
+            foreach ($trackingEventsMapping as $trackingEventMapping) {
+               
+                if ($PODeventArray['TrackingMsg'] == $trackingEventMapping->TrackingMsg) {
+
+                    $PODeventsArray[$offset]['TrackingMasterCode'] = $trackingEventMapping->TrackingMasterCode;
+                   
+                    $PODeventsArray[$offset]['TrackingMasterEventDescription'] = $trackingEventsMasterArray[$trackingEventMapping->TrackingMasterCode];
+                   
+                    $PODeventsArray[$offset]['OurEventCode'] = $trackingEventMapping->OurEventCode;
+
+                    $PODeventsArray[$offset]['EventDescription'] = $trackingEventMapping->EventDescription;
+
+                    break;
+                }
+            }
+            $offset++;
+        }
+        po($PODeventsArray);
     }
 }
