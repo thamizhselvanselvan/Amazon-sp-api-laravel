1a20209465344b6bc3f44b26cf545982403c76b7
amitpms <nu.palmate@gmail.com>
2022-04-04 19:00:13 +0530

    rename header amazon other to amazon.in

--- resources/views/amazonOtherProduct/index.blade.php
+++ resources/views/amazonOtherProduct/index.blade.php
@@ -1,9 +1,9 @@
 @extends('adminlte::page')
 
-@section('title', 'Amazon other product')
+@section('title', 'Amazon.com product')
 
 @section('content_header')
-<h1 class="m-0 text-dark">Amazon Other Products</h1>
+<h1 class="m-0 text-dark">Amazon.com Products</h1>
 @stop
 
 @section('css')
