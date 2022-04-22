23ed1870c9676995adbb8cf91cbdc3ea6f155c9a
amitpms <nu.palmate@gmail.com>
2022-04-22 18:02:05 +0530

    upload file to DO Server

--- app/Console/Commands/BoeUploadServerToDo.php
+++ app/Console/Commands/BoeUploadServerToDo.php
@@ -40,27 +40,20 @@ class BoeUploadServerToDo extends Command
      */
     public function handle()
     {
-        $year = date('Y');
-        $month = date('F');
-        // dd($month);
-        $files_path = BOE::where('do', 0)->limit(1)->get(['id','download_file_path']);
-        // dd($files_path);
-        // dd($boe[0]['download_file_path']);
-        // exit;
-        // BOE::where('do', 0)->chunk(5, function ($files_path) use($year, $month){
 
-
+        BOE::where('do', 0)->chunk(5, function ($files_path) {
 
             foreach ($files_path as $file_path) {
-                
+
                 $file_path_array = explode('/', $file_path->download_file_path);
                 $file_name = $file_path_array[count($file_path_array) - 1];
                 $file = storage_path('app/' . $file_path->download_file_path);
                 Storage::disk('do')->put($file_path->download_file_path, file_get_contents($file));
                 BOE::where('id', $file_path->id)->update(['do' => 1]);
-                
+                if (is_file($file)) {
+                    unlink($file);
+                }
             }
-            echo 'success';
-        // });
+        });
     }
 }
