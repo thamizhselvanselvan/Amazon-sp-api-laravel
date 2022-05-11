f46235b00f9053031f94c7fc245dae496987964e
sanju <epsilon.palmate@gmail.com>
2022-05-11 20:51:05 +0530

    Added inventory shipment price column.

--- app/Models/Inventory/Shipment.php
+++ app/Models/Inventory/Shipment.php
@@ -9,7 +9,7 @@ class Shipment extends Model
 {
     use HasFactory;
     protected $connection = 'in';
-    protected $fillable = ['source_id','asin','Ship_id'];
+    protected $fillable = ['source_id','asin','ship_id','item_name','quantity','price'];
 
     public function sources() {
         return $this->hasOne(Source::class, 'id', 'source_id');
