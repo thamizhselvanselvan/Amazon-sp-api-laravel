fc1852548e9c7fb8256eb81a22cc21e22fc85975
sanju <epsilon.palmate@gmail.com>
2022-05-12 19:16:52 +0530

    Added Inventory Models

---
+++ app/Models/Inventory/Source.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models\Inventory;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Source extends Model
+{
+    use HasFactory;
+    protected $connection = 'in';
+    protected $fillable = ['name'];
+
+    
+    public function shipments()
+    {
+        return $this->hasMany(Shipment::class);
+    }
+}
