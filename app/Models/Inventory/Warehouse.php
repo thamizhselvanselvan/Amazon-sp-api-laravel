1731e768fe4341c243c1b1e5b5c426d742643219
sanju <epsilon.palmate@gmail.com>
2022-05-12 19:26:04 +0530

    Added Invenory Models

---
+++ app/Models/Inventory/Warehouse.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models\Inventory;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Warehouse extends Model
+{
+    use HasFactory;
+    use HasFactory;
+    protected $connection = 'in';
+
+    protected $fillable = [
+        'name',
+        'address_1',
+        'address_2',
+        'city',
+        'state',
+        'country',
+        'pin_code',
+        'contact_person_name',
+        'phone_number',
+        'email',
+    ];
+
+    public function racks()
+    {
+        return $this->hasMany(Racks::class);
+    }
+}
