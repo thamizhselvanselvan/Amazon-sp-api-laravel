2dd2d9acad3a57d9c2de2b66c45416d000f33b9c
sanju <epsilon.palmate@gmail.com>
2022-05-26 16:37:07 +0530

    Displayed Region details In warehouse and Vendors

--- app/Models/Inventory/Warehouse.php
+++ app/Models/Inventory/Warehouse.php
@@ -30,16 +30,16 @@ class Warehouse extends Model
     }
     public function countrys()
     {
-        return $this->hasOne(Country::class, 'id', 'country_id');
+        return $this->hasOne(Country::class, 'id', 'country');
     }
    
     public function states()
     {
-        return $this->hasOne(State::class, 'id', 'country_id');
+        return $this->hasOne(State::class, 'id', 'country');
     }
 
     public function citys()
     {
-        return $this->hasOne(City::class ,'id','state_id');
+        return $this->hasOne(City::class ,'id','state');
     }
 }
