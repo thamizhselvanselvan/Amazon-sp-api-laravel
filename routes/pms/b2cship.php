eaaaf2d3dc1013f02e34c426d4907808f9fa89e6
vikesh <zeta.palmate@gmail.com>
2022-05-30 12:16:13 +0530

    create tabs for TrackingAPI, Bombino Status, Delivery Status, Misc Status and Add Active|Inactive tabs for Tracking API, Bombino Status, Delivery Status, and Booking kyc status

--- routes/pms/b2cship.php
+++ routes/pms/b2cship.php
@@ -15,5 +15,10 @@ Route::get('bombion/packet-activities','B2cship\BombinoPacketActivitiesControlle
 Route::get('bombion/update-packet-details','B2cship\BombinoPacketActivitiesController@UpdatePacketDetails');
 Route::get('bombion/csv-export','B2cship\BombinoPacketActivitiesController@ExportToCSV');
 
+Route::get('b2cship/Trackingtab','B2cship\B2cshipDashboardController@showDashboard');
+Route::get('b2cship/Bombinotab','B2cship\B2cshipDashboardController@BombinoStatus');
+Route::get('b2cship/Deliverytab','B2cship\B2cshipDashboardController@BlueDartAndDeliveryStatus');
+Route::get('b2cship/Misctab','B2cship\B2cshipDashboardController@BookingAndKycStatusDetails');
+Route::get('b2cship/BombinoStatusInactive','B2cship\B2cshipDashboardController@BombinoInactive');
 
 
