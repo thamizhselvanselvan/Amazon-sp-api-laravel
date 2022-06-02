4573dcf425be011c7d4314fb5d00d86de24b39a4
amitpms <nu.palmate@gmail.com>
2022-06-02 13:18:04 +0530

    order duplicate issue fixed

--- app/Console/Commands/SellerOrdersImport.php
+++ app/Console/Commands/SellerOrdersImport.php
@@ -62,19 +62,19 @@ class SellerOrdersImport extends Command
         R::setup("mysql:host=$host;dbname=$dbname;port=$port", $username, $password);
         // $aws_data = Aws_credential::with('mws_region')->where('dump_order', 1)->where('verified', 1)->get();
         $aws_data = OrderSellerCredentials::where('dump_order', 1)->get();
-        
+
         foreach ($aws_data as $aws_value) {
-            
+
             $awsId  = $aws_value['id'];
             // $awsAuth_code = $aws_value['auth_code'];
             $awsCountryCode = $aws_value['country_code'];
             $this->seller_id = $aws_value['seller_id'];
             $bb_aws_cred = Aws_credential::where('seller_id', $this->seller_id)->get();
             $awsAuth_code = $bb_aws_cred[0]->auth_code;
-// po($this->seller_id);
+            // po($this->seller_id);
             $this->SelectedSellerOrder($awsId, $awsCountryCode, $awsAuth_code);
         }
-        // exit;
+        exit;
     }
 
     public function SelectedSellerOrder($awsId, $awsCountryCode, $awsAuth_code)
@@ -96,15 +96,13 @@ class SellerOrdersImport extends Command
             next_token_exist:
             $results = $apiInstance->getOrders($marketplace_ids, $createdAfter, $created_before = null, $last_updated_after = null, $last_updated_before = null, $order_statuses = null, $fulfillment_channels = null, $payment_methods = null, $buyer_email = null, $seller_order_id = null, $max_results_per_page, $easy_ship_shipment_statuses = null, $next_token, $amazon_order_ids = null, $actual_fulfillment_supply_source_id = null, $is_ispu = null, $store_chain_store_id = null, $data_elements = null)->getPayload();
             $next_token = $results['next_token'];
-             $this->OrderDataFormating($results);
-            
-            if(isset($next_token))
-            {
+            $this->OrderDataFormating($results);
+
+            if (isset($next_token)) {
                 goto next_token_exist;
             }
             $orders = '';
             $amazon_order_id = '';
-
         } catch (Exception $e) {
 
             Log::warning('Exception when calling OrdersApi->getOrders: ', $e->getMessage(), PHP_EOL);
@@ -112,43 +110,42 @@ class SellerOrdersImport extends Command
     }
 
     public function OrderDataFormating($results)
-    {   
+    {
         $result_data = $results->getOrders();
         $result_data = json_decode(json_encode($result_data));
-
+        $count = 0;
         foreach ($result_data as $resultkey => $result) {
-            
+
+            // po($result);
+            // exit;
             $orders = R::dispense('orders');
             $amazon_order_details = [];
             $orders->our_seller_identifier = $this->seller_id;
-            
+            $amazon_order_id = '';
             foreach ((array)$result as $detailsKey => $details) {
 
 
                 $detailsKey = lcfirst($detailsKey);
                 if (is_Object($details)) {
-                    
+
                     $amazon_order_details[$detailsKey] = json_encode($details);
                     $orders->{$detailsKey} = json_encode($details);
-
                 } else if (is_array($details)) {
 
                     $amazon_order_details[$detailsKey] = json_encode($details);
                     $orders->{$detailsKey} = json_encode($details);
-
                 } else {
 
                     $id = substr($detailsKey, -2);
-                    if($id == 'Id'){
-                        $detailsKey = str_replace("Id","Identifier",$detailsKey);
+                    if ($id == 'Id') {
+                        $detailsKey = str_replace("Id", "Identifier", $detailsKey);
                     }
                     if ($detailsKey == 'amazonOrderIdentifier') {
 
                         $amazon_order_id = $details;
                         $amazon_order_details['amazon_order_identifier'] = $details;
                         $orders->amazon_order_identifier = $details;
-
-                    } 
+                    }
                     // else if ($detailsKey == 'marketplaceId') {
 
                     //     $amazon_order_details['marketplace'] = $details;
@@ -159,35 +156,42 @@ class SellerOrdersImport extends Command
                     //     $amazon_order_details['seller_order_identifier'] = $details;
                     //     $orders->seller_order_identifier = $details;
                     // }
-                     else {
+                    else {
 
                         $amazon_order_details[$detailsKey] = (string)$details;
                         $orders->{$detailsKey} = (string)$details;
-
                     }
                 }
             }
-            $data = DB::connection('order')->select("select id from orders where EXISTS( select id from orders where amazon_order_identifier = '$amazon_order_id')");
+            
+            //$amazon_order_id = '407-0297568-739477566';
 
-            // $data =[];
+            $data = DB::connection('order')->select("select id, amazon_order_identifier from orders where amazon_order_identifier = '$amazon_order_id'");
+          
             if (array_key_exists(0, $data)) {
+
+                $count++;
 
                 $dataCheck = 1;
                 $id = $data[0]->id;
-                $update_orders = R::load('orders',$id);
-                
-                foreach($amazon_order_details as $key => $value)
-                {   
+                $update_orders = R::load('orders', $id);
+                $update_orders->our_seller_identifier = $this->seller_id;
+                foreach ($amazon_order_details as $key => $value) {
                     $update_orders->{$key} = $value;
                 }
                 $update_orders->updatedat = now();
+                // dd($update_orders, 'update');
                 R::store($update_orders);
-            }
-            else{
+            } else {
                 $orders->createdat = now();
+                // dd($orders);
                 R::store($orders);
             }
         }
+        echo $count;
+        echo "<hr>";
+        
+
         return true;
     }
 }
