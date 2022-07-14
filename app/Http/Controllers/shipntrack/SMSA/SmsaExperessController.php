7c57dfb9fbfda76497a7d145b52792655c29f070
amitpms <nu.palmate@gmail.com>
2022-07-14 14:59:11 +0530

    smsa migration created

--- app/Http/Controllers/shipntrack/SMSA/SmsaExperessController.php
+++ app/Http/Controllers/shipntrack/SMSA/SmsaExperessController.php
@@ -29,55 +29,59 @@ class SmsaExperessController extends Controller
 
         $datas = preg_split('/[\r\n| |:|,]/', $tracking_id, -1, PREG_SPLIT_NO_EMPTY);
         $datas = array_unique($datas);
-        
-        // dd($datas);
-        $password = 'Bom@7379';
-        $url = "http://track.smsaexpress.com/SECOM/SMSAwebService.asmx";
 
+        $tracking_details = [];
         foreach ($datas as $awbNo) {
 
-            // $awbNo = '290314335017';
-            $xmlRequest = "<?xml version='1.0' encoding='utf-8'?>
-            <soap:Envelope xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema' xmlns:soap='http://schemas.xmlsoap.org/soap/envelope/'>
-                <soap:Body>
-                    <getTracking xmlns='http://track.smsaexpress.com/secom/'>
-                        <awbNo>$awbNo</awbNo>
-                        <passkey>$password</passkey>
-                    </getTracking>
-                </soap:Body>
-            </soap:Envelope>";
-
-            $headers = array(
-                'Content-type: text/xml',
-            );
-
-            $ch = curl_init();
-            //setting the curl options
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-            curl_setopt($ch, CURLOPT_POSTFIELDS,  $xmlRequest);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_VERBOSE, 0);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-            $data = curl_exec($ch);
-
-            $plainXML = $this->mungXML(trim($data));
-            $arrayResult = json_decode(json_encode(SimpleXML_Load_String($plainXML, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
-
-            $arrayResult = $arrayResult['soap_Body']['getTrackingResponse']['getTrackingResult']['diffgr_diffgram'];
-            if (array_key_exists('NewDataSet', $arrayResult)) {
-
-                po($arrayResult['NewDataSet']['Tracking']);
-                echo "<hr>";
-            } else {
-                echo "Invalid Awb No. ". $awbNo;
-            }
+            $tracking_details[] = $this->TrackingResponse($awbNo);
+            
         }
     }
 
+    public function TrackingResponse($awbNo)
+    {
+        $password = 'Bom@7379';
+        $url = "http://track.smsaexpress.com/SECOM/SMSAwebService.asmx";
+
+        $xmlRequest = "<?xml version='1.0' encoding='utf-8'?>
+        <soap:Envelope xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema' xmlns:soap='http://schemas.xmlsoap.org/soap/envelope/'>
+            <soap:Body>
+                <getTracking xmlns='http://track.smsaexpress.com/secom/'>
+                    <awbNo>$awbNo</awbNo>
+                    <passkey>$password</passkey>
+                </getTracking>
+            </soap:Body>
+        </soap:Envelope>";
+
+        $headers = array(
+            'Content-type: text/xml',
+        );
+
+        $ch = curl_init();
+        //setting the curl options
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_POSTFIELDS,  $xmlRequest);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_VERBOSE, 0);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        $data = curl_exec($ch);
+
+        $plainXML = $this->mungXML(trim($data));
+        $arrayResult = json_decode(json_encode(SimpleXML_Load_String($plainXML, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
+
+        $arrayResult = $arrayResult['soap_Body']['getTrackingResponse']['getTrackingResult']['diffgr_diffgram'];
+        if (array_key_exists('NewDataSet', $arrayResult)) {
+
+            return $arrayResult['NewDataSet']['Tracking'];
+        } else {
+
+            // echo "Invalid Awb No. ". $awbNo;
+        }
+    }
     public function mungXML($xml)
     {
         $obj = SimpleXML_Load_String($xml);
