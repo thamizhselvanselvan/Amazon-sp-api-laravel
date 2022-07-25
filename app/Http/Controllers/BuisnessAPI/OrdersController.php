030c8434596f1e1fd78bae8f88e72102c30deb74
sanju <epsilon.palmate@gmail.com>
2022-07-25 20:18:48 +0530

    Order API data fetched.

--- app/Http/Controllers/BuisnessAPI/OrdersController.php
+++ app/Http/Controllers/BuisnessAPI/OrdersController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers\BuisnessAPI;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Services\AWS_Business_API\AWS_POC\Orders;
 
 class OrdersController extends Controller
 {
     public function index()
     {
+        $ApiCall = new Orders();
+        $data = $ApiCall->getOrders();
+        dd($data);
         return view('buisnessapi.orders.index');
-        
     }
+    
 }
