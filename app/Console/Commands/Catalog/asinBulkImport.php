71cf3236155c3139eb830cc4545113fb392eb7ee
amitpms <nu.palmate@gmail.com>
2022-08-27 12:33:02 +0530

    bulk asin import fixed

--- app/Console/Commands/Catalog/asinBulkImport.php
+++ app/Console/Commands/Catalog/asinBulkImport.php
@@ -49,7 +49,7 @@ class asinBulkImport extends Command
 
         $user_id = $this->argument('user_id');
         $sources = explode(',', $this->option('source'));
-        
+
         $path = 'AsinMaster/asin.csv';
         $csv = Reader::createFromPath(Storage::path($path), 'r');
         $csv->setDelimiter(",");
@@ -59,38 +59,34 @@ class asinBulkImport extends Command
         $asin = [];
         $count = 0;
 
-        foreach($sources as $key1 => $this->country)
-        {
+        foreach ($sources as $key1 => $this->country) {
             // $country [] = $country;
             foreach ($csv as $key => $record) {
                 $country_code = strtoupper($this->country);
                 $seller_id = $source[$country_code];
                 $asin = $record['ASIN'];
-    
+
                 $asin_details[] = [
-    
+
                     'asin' => $asin,
                     'user_id' => $user_id,
                     // 'source' => $record['Source'],
                 ];
-    
+
                 if ($count == 999) {
-                    
-                    $model_name = table_model_create(country_code:$this->country, model:'Asin_source', table_name:'asin_source_');
+
+                    $model_name = table_model_create(country_code: $this->country, model: 'Asin_source', table_name: 'asin_source_');
                     $model_name->upsert($asin_details, ['user_asin_unique'], ['asin']);
                     $count = 0;
-                    $asin = [];
+                    $asin_details = [];
                 }
                 $count++;
             }
-            $table_name = table_model_create(country_code:$this->country, model:'Asin_source', table_name:'asin_source_');
+            $table_name = table_model_create(country_code: $this->country, model: 'Asin_source', table_name: 'asin_source_');
             $table_name->upsert($asin_details, ['user_asin_unique'], ['asin']);
-        
-            // $push_to_bb->PushAsinToBBTable(product: $product, product_lowest_price: $product_lowest_price, country_code: $country_code);
-            
-            // $table_name = table_model_create(country_code:$this->country, model:'Asin_source', table_name:'asin_source_');
+
             $asins = $table_name->where('status', 0)->get(['asin', 'user_id']);
-            
+
             $count = 0;
             $asin_source = [];
             $class = 'catalog\AmazonCatalogImport';
@@ -101,17 +97,16 @@ class asinBulkImport extends Command
 
                     $asin_source = [];
                     $count = 0;
-                } 
+                }
                 $asin_source[] = [
                     'asin' => $asin->asin,
                     'source' => $this->country,
                     'seller_id' => $asin->user_id
                 ];
                 $count++;
-            } 
-            
+            }
+
             jobDispatchFunc($class, $asin_source, 'catalog');
         }
-        
     }
 }
