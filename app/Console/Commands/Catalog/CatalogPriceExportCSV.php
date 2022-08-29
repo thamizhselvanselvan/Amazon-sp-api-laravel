b333e800dd31940b522e558f379e840e9dcae96a
amitpms <nu.palmate@gmail.com>
2022-08-29 16:20:16 +0530

    command added

--- app/Console/Commands/Catalog/CatalogPriceExportCSV.php
+++ app/Console/Commands/Catalog/CatalogPriceExportCSV.php
@@ -52,5 +52,106 @@ class CatalogPriceExportCSV extends Command
      */
     public function handle()
     {
+        $this->country_code = $this->option('country_code');
+
+        $chunk = 10000;
+
+        $exportFilePath = "excel/downloads/catalog_price/$this->country_code/" . $this->country_code . "_CatalogPrice";
+        $deleteFilePath = "app/excel/downloads/catalog_price/" . $this->country_code;
+
+        // if (file_exists(storage_path($deleteFilePath))) {
+        //     $path = storage_path($deleteFilePath);
+        //     $files = (scandir($path));
+        //     foreach ($files as $key => $file) {
+        //         if ($key > 1) {
+        //             unlink($path . '/' . $file);
+        //         }
+        //     }
+        // }
+
+        $record_per_csv = 1000000;
+        $chunk = 100000;
+
+        $this->check = $record_per_csv / $chunk;
+
+        if ($this->country_code == 'IN') {
+
+            $headers = ['asin', 'available', 'in_price', 'weight', 'ind_to_uae', 'ind_to_sg', 'ind_to_sa', 'price_updated_at'];
+            $csv_header = ['Asin', 'Available', 'India Price', 'Weight(kg)', 'IND To UAE', 'IND To Singapore ', 'IND To Saudi', 'Updated At'];
+            PricingIn::select($headers)->chunk($chunk, function ($records) use ($exportFilePath, $csv_header, $chunk) {
+
+                $this->CreateCsvFile($csv_header, $records, $exportFilePath);
+                //pusher
+            });
+        } elseif ($this->country_code == 'US') {
+
+            $headers = ['asin', 'available', 'weight', 'us_price', 'usa_to_in_b2b', 'usa_to_in_b2c', 'usa_to_uae', 'usa_to_sg', 'price_updated_at'];
+            $csv_header = ['Asin', 'Available', 'Weight', 'US Price', 'USA To IND B2B', 'USA To IND B2C', 'USA To UAE', 'USA To Singapore', 'Updated At'];
+            PricingUs::select($headers)->chunk($chunk, function ($records) use ($exportFilePath, $csv_header, $chunk) {
+
+                $this->CreateCsvFile($csv_header, $records, $exportFilePath);
+                //pusher
+            });
+        }
+
+        $path = "excel/downloads/catalog_price/" . $this->country_code;
+        $path = Storage::path($path);
+        $files = (scandir($path));
+
+        $filesArray = [];
+        foreach ($files as $key => $file) {
+            if ($key > 1) {
+                if (str_contains($file, '.mosh')) {
+                    $new_file_name = str_replace('.csv.mosh', '.csv', $file);
+                    rename($path . '/' . $file, $path . '/' . $new_file_name);
+                }
+            }
+        }
+
+        $zip = new ZipArchive;
+        $path = "excel/downloads/catalog_price/" . $this->country_code . "/zip/" . $this->country_code . "_CatalogPrice.zip";
+        $file_path = Storage::path($path);
+
+        if (!Storage::exists($path)) {
+            Storage::put($path, '');
+        }
+
+        if ($zip->open($file_path, ZipArchive::CREATE) === TRUE) {
+            foreach ($this->totalFile as $key => $value) {
+                $path = Storage::path('excel/downloads/catalog_price/' . $this->country_code . '/' . $value);
+                $relativeNameInZipFile = basename($path);
+                $zip->addFile($path, $relativeNameInZipFile);
+            }
+            $zip->close();
+        }
+    }
+
+    public function CreateCsvFile($csv_header, $records, $exportFilePath)
+    {
+        if ($this->count == 1) {
+            if (!Storage::exists($exportFilePath . $this->fileNameOffset . '.csv.mosh')) {
+                Storage::put($exportFilePath . $this->fileNameOffset . '.csv.mosh', '');
+            }
+            $this->totalFile[] = $this->country_code . "_CatalogPrice" . $this->fileNameOffset . '.csv';
+            $this->writer = Writer::createFromPath(Storage::path($exportFilePath . $this->fileNameOffset . '.csv.mosh'), "w");
+            $this->writer->insertOne($csv_header);
+        }
+
+        $records = $records->toArray();
+        $records = array_map(function ($datas) {
+            return (array) $datas;
+        }, $records);
+
+        $this->writer->insertall($records);
+
+        if ($this->check == $this->count) {
+            $this->fileNameOffset++;
+            $this->count = 1;
+        } else {
+            ++$this->count;
+        }
+
+        return true;
+        //
     }
 }
--- app/Console/Commands/Catalog/CatalogPriceImport.php
+++ app/Console/Commands/Catalog/CatalogPriceImport.php
@@ -47,5 +47,148 @@ class CatalogPriceImport extends Command
      */
     public function handle()
     {
+        $product = '';
+        $product_lp = '';
+        $seller_id = '';
+
+        // $source = buyboxCountrycode();
+        $source = [
+            'IN' => 39
+            // 'US' => 40
+        ];
+        $price_convert = new PriceConversion();
+        $chunk = 10;
+        foreach ($source as $country_code => $seller_id) {
+
+            $country_code_lr = strtolower($country_code);
+
+            $product_seller_details = 'bb_product_' . $country_code_lr . 's_seller_details';
+            $product_lp = 'bb_product_' . $country_code_lr . 's_lp_offers';
+
+            // $catalog_table = 'catalog' . $country_code_lr . 's';
+            // AsinDestination::select('asin_destinations.asin', "$catalog_table.package_dimensions")
+            //     ->where('asin_destinations.destination', $country_code)
+            //     ->join($catalog_table, 'asin_destinations.asin', '=', "$catalog_table.asin")
+
+            $table_name = 'catalog' . $country_code_lr . 's';
+            $modal_table = table_model_create(country_code: $this->country_code, model: 'Catalog', table_name: 'catalognew');
+
+            $modal_table->select(['asin', 'dimensioins'])->chunk($chunk, function ($data) use ($seller_id, $country_code_lr, $product_seller_details, $product_lp, $price_convert) {
+
+                $pricing = [];
+                $pricing_in = [];
+                $asin_details = [];
+                $listing_price_amount = '';
+
+                $asin_array = [];
+                foreach ($data as $value) {
+                    $weight = '0.5';
+
+                    if (isset(json_decode($value->dimensions)[0]->package->weight->value)) {
+                        $weight = json_decode($value->dimensions)[0]->package->weight->value;
+                    }
+
+                    $a = $value->asin;
+                    $calculated_weight[$a] =  $weight;
+                    $asin_array[] = "'$a'";
+                }
+
+                $asin = implode(',', $asin_array);
+
+                $asin_price = DB::connection('buybox')
+                    ->select("SELECT PPO.asin, LP.available,
+                    GROUP_CONCAT(PPO.is_buybox_winner) as is_buybox_winner,
+                    group_concat(PPO.listingprice_amount) as listingprice_amount,
+                    group_concat(PPO.updated_at) as updated_at
+                    FROM $product_seller_details as PPO
+                    JOIN $product_lp as LP
+                        WHERE PPO.asin = LP.asin
+                        AND PPO.asin IN ($asin)
+                        GROUP BY PPO.asin
+                    ");
+
+                foreach ($asin_price as $value) {
+
+                    $buybox_winner = explode(',', $value->is_buybox_winner);
+                    $listing_price = explode(',', $value->listingprice_amount);
+                    $updated_at = explode(',', $value->updated_at);
+
+                    $asin_name = $value->asin;
+                    $available = $value->available;
+                    $packet_weight = $calculated_weight[$asin_name];
+
+                    foreach ($buybox_winner as $key =>  $value1) {
+
+                        $price = $country_code_lr . '_price';
+                        if ($value1 == '1') {
+
+                            $listing_price_amount = $listing_price[$key];
+
+                            $asin_details =
+                                [
+                                    'asin' =>  $asin_name,
+                                    'available' => $available,
+                                    $price => $listing_price_amount,
+                                    'price_updated_at' => max($updated_at),
+                                ];
+                            break 1;
+                        } else {
+
+                            $listing_price_amount =  min($listing_price);
+                            $asin_details =
+                                [
+                                    'asin' =>  $asin_name,
+                                    'available' => $available,
+                                    $price => $listing_price_amount,
+                                    'price_updated_at' =>  max($updated_at),
+                                ];
+                        }
+                    }
+                    if ($country_code_lr == 'us') {
+
+                        $price_in_b2c = $price_convert->USAToINDB2C($packet_weight, $listing_price_amount);
+
+                        $price_in_b2b = $price_convert->USAToINDB2B($packet_weight, $listing_price_amount);
+
+                        $price_ae = $price_convert->USATOUAE($packet_weight, $listing_price_amount);
+
+                        $price_sg =  $price_convert->USATOSG($packet_weight, $listing_price_amount);
+
+                        $price_us_source = [
+                            'usa_to_in_b2c' => $price_in_b2c,
+                            'usa_to_in_b2b' => $price_in_b2b,
+                            'usa_to_uae' => $price_ae,
+                            'usa_to_sg' => $price_sg,
+                            'weight' => $packet_weight
+                        ];
+
+                        $pricing[] = [...$asin_details, ...$price_us_source];
+                    } elseif ($country_code_lr == 'in') {
+
+                        $packet_weight_kg = poundToKg($packet_weight);
+
+                        $price_saudi = $price_convert->INDToSA($packet_weight_kg, $listing_price_amount);
+                        $price_singapore = $price_convert->INDToSG($packet_weight_kg, $listing_price_amount);
+                        $price_uae = $price_convert->INDToUAE($packet_weight_kg, $listing_price_amount);
+
+                        $destination_price = [
+                            'ind_to_uae' => $price_uae,
+                            'ind_to_sg' => $price_singapore,
+                            'ind_to_sa' => $price_saudi,
+                            'weight' => $packet_weight_kg
+                        ];
+                        $pricing_in[] = [...$asin_details, ...$destination_price];
+                    }
+                }
+                if ($country_code_lr == 'us') {
+
+                    PricingUs::upsert($pricing, 'unique_asin',  ['asin', 'available', 'weight', 'us_price', 'usa_to_in_b2b', 'usa_to_in_b2c', 'usa_to_uae', 'usa_to_sg', 'price_updated_at']);
+                } elseif ($country_code_lr == 'in') {
+
+                    PricingIn::upsert($pricing_in, 'asin_unique', ['asin', 'available', 'in_price', 'weight', 'ind_to_uae', 'ind_to_sg', 'ind_to_sa', 'price_updated_at']);
+                }
+                // exit;
+            });
+        }
     }
 }
