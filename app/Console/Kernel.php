17362af7ad78bdd0f1aa7f596b2e65a9c0a82ea9
amitpms <nu.palmate@gmail.com>
2022-09-01 17:01:28 +0530

    catalog import removed from staging

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -41,7 +41,7 @@ class Kernel extends ConsoleKernel
         if (app()->environment() === 'staging') {
             // $schedule->command('pms:sellers-orders-import')->everyTenMinutes()->thenPing('http://beats.envoyer.io/heartbeat/uoR2oSENfKrIC4z');
 
-            $schedule->command('mosh:catalog-amazon-import')->everyTenMinutes()->thenPing('http://beats.envoyer.io/heartbeat/uoR2oSENfKrIC4z');
+            // $schedule->command('mosh:catalog-amazon-import')->everyTenMinutes()->thenPing('http://beats.envoyer.io/heartbeat/uoR2oSENfKrIC4z');
         }
 
         if (app()->environment() === 'local') {
