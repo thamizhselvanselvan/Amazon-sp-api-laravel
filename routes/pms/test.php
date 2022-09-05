cbd39a3bc4282b0cb49eb03d8ee753f59d7ea46b
amitpms <nu.palmate@gmail.com>
2022-09-05 11:50:15 +0530

    test route added to download catalog price

--- routes/pms/test.php
+++ routes/pms/test.php
@@ -6,10 +6,20 @@ use Illuminate\Support\Facades\DB;
 use App\Models\Catalog\Asin_master;
 use Illuminate\Support\Facades\Route;
 use App\Models\seller\AsinMasterSeller;
+use Illuminate\Support\Facades\Storage;
 use App\Models\seller\SellerAsinDetails;
 use Illuminate\Support\Facades\Response;
 use Symfony\Component\CssSelector\XPath\Extension\FunctionExtension;
 
+
+Route::get('in', function () {
+    return Storage::download('excel\downloads\catalog_price\IN\Priority1\IN_CatalogPrice0.csv');
+});
+
+Route::get('us', function () {
+
+    return Storage::download('excel\downloads\catalog_price\US\Priority1\US_CatalogPrice0.csv');
+});
 Route::get("test/{country_code}", function ($country_code) {
 
 
