f6ebf3af22bea3e46d17c22d8b2f1db3684e29eb
vikesh <zeta.palmate@gmail.com>
2022-09-08 16:05:56 +0530

    separate catalog dimensions column.

--- app/Services/SP_API/API/NewCatalog.php
+++ app/Services/SP_API/API/NewCatalog.php
@@ -124,6 +124,7 @@ class NewCatalog
                 $queue_data[$key]['seller_id'] = $seller_id;
                 $queue_data[$key]['source'] = $country_code;
                 foreach ($record as $key1 => $value) {
+
                     if ($key1 == 'summaries') {
                         foreach ($value[0] as $key2 => $value2) {
                             $key2 = str_replace('marketplaceId', 'marketplace', $key2);
@@ -134,10 +135,14 @@ class NewCatalog
                         $queue_data[$key][$key1] = $this->returnDataType($value);
                     }
                     if ($key1 == 'dimensions') {
+
                         if (array_key_exists('package', (array)$value[0])) {
+
                             foreach ($value[0]->package as $key3 => $value3) {
+
                                 $queue_data[$key][$key3] = $value3->value;
                                 if ($key3 == 'height' || $key3 == 'width' || $key3 == 'length') {
+                                    
                                     $queue_data[$key]['unit'] = $value3->unit;
                                 }
                                 if ($key3 == 'weight') {
