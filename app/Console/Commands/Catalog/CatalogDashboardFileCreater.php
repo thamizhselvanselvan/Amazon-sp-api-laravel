b5d251d322c5e368478e1d568c1e4674289777c5
vikesh <zeta.palmate@gmail.com>
2022-09-09 22:15:21 +0530

    Fixed catalog dashboard count.

--- app/Console/Commands/Catalog/CatalogDashboardFileCreater.php
+++ app/Console/Commands/Catalog/CatalogDashboardFileCreater.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands\Catalog;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
@@ -40,54 +41,75 @@ class CatalogDashboardFileCreater extends Command
     public function handle()
     {
         $sources = ['IN', 'US'];
-        $total_asin = 0;
-        $total_catalog = 0 ;
-        $priority_wise = [];
-        $Total_catalog = [];
         $record_arrays = [];
-        $delist_asin = [];
-        $cat_price = [];
         $dbname = config('database.connections.buybox.database');
         foreach($sources as $source){
-            $table_name = table_model_create(country_code:$source, model:'Asin_destination', table_name:'asin_destination_');
+            
+            $asin_priority = [1 => 0,2 => 0,3 => 0];
+            $catalog = [1 => 0, 2 => 0, 3 => 0];
+            $asin_delist = [1 => 0,2 => 0,3 => 0];
+            $asin_catalog_price = [1 => 0,2 => 0,3 => 0];
             $source = strtolower($source);
             $destination_table = "asin_destination_${source}s";
             $catalog_table = "catalognew${source}s";
-            // $total_asin += $table_name->get('asin')->count();
-            // $total_catalog += DB::connection('catalog')->table($catalog_table )->count('asin');
-
             $buybox_table = "bb_product_${source}s_lp_offers";
             $catalog_price = "pricing_${source}s";
 
-            for($priority=1; $priority<=3; $priority++){
-
-                $priority_wise []= $table_name->where('priority', $priority)->get('asin')->count();
-                $Total_catalog []= $table_name->where($destination_table.'.priority', $priority)
-                ->join($catalog_table, $destination_table.'.asin', '=', $catalog_table.'.asin')->count();
-
-                $delist_asin []= $table_name->where($destination_table.'.priority', $priority)
-                ->where($buybox_table.'.delist', 1)
-                ->join($dbname.".${buybox_table}", $destination_table.'.asin', '=', $buybox_table.'.asin')->count();
-                
-                $cat_price []= $table_name->where($destination_table.'.priority', $priority)
-                ->join($catalog_price, $destination_table.'.asin', '=', $catalog_price.'.asin')->count();
-                
-
+            $priority_wise = DB::connection('catalog')
+            ->select("SELECT count(asin) as priority_wise, priority from ${destination_table} 
+            group by priority");
+            foreach($priority_wise as $priority)
+            {
+                $value = $priority->priority;
+                $asin_priority[$value] = $priority->priority_wise;
             }
-            $record_arrays []= [
-                'source' => $source,
-                'priority_wise_asin' => $priority_wise,
-                'catalog' => $Total_catalog,
-                'delist_asin' => $delist_asin,
-                'catalog_price' => $cat_price,
-            ];
             
-            $priority_wise = [];
-            $Total_catalog = [];
-            $delist_asin = [];
-            $cat_price = [];
+            $Total_catalogs = DB::connection('catalog')
+            ->select("SELECT count(${destination_table}.asin) as asin_catalog ,${destination_table}.priority from ${destination_table}
+            join ${catalog_table}
+            where ${destination_table}.asin = ${catalog_table}.asin
+            group by ${destination_table}.priority
+            ");
+            
+            foreach($Total_catalogs as $total_catalog)
+            {
+                $cat = $total_catalog->priority;
+                $catalog[$cat] = $total_catalog->asin_catalog;
+            }
+
+            $delist_asins = DB::connection('catalog')
+            ->select("SELECT count(${destination_table}.asin) as asin_delist, ${destination_table}.priority from ${destination_table}
+            join ${dbname}.${buybox_table}
+            where $destination_table.asin = $buybox_table.asin
+            and ${buybox_table}.delist = 1
+            group by ${destination_table}.priority
+            ");
+
+            foreach($delist_asins as $delist_asin){
+                $delist = $delist_asin->priority;
+                $asin_delist[$delist] = $delist_asin->asin_delist;
+            }
+            
+            $cat_prices =  DB::connection('catalog')
+            ->select("SELECT count(${destination_table}.asin) as catalog_price, ${destination_table}.priority from ${destination_table}
+            join ${catalog_price}
+            where ${destination_table}.asin = ${catalog_price}.asin
+            group by ${destination_table}.priority
+            ");
+            foreach($cat_prices as $cat_price){
+                $price = $cat_price->priority;
+                $asin_catalog_price[$price] = $cat_price->catalog_price;
+            }
+                
+            $record_arrays []  = [
+            'priority_wise_asin' => $asin_priority,
+            'catalog' => $catalog,
+            'delist_asin' => $asin_delist,
+            'catalog_price' => $asin_catalog_price,
+            ];
+
         }
-        $cat_dashboard_file = "excel/downloads/catalog-dashboard-file.json";
+        // $cat_dashboard_file = "excel/downloads/catalog-dashboard-file.json";
         $cat_dashboard_file = "Dashboard/catalog-dashboard-file.json";
         if(!Storage::exists($cat_dashboard_file)){
             Storage::put($cat_dashboard_file, '');
