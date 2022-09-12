dc5c4474cd4b0f9fe027a00fc949422fc73764d7
amitpms <nu.palmate@gmail.com>
2022-09-12 15:48:56 +0530

    catalog import

--- app/Console/Commands/Catalog/CatalogDashboardFileCreater.php
+++ app/Console/Commands/Catalog/CatalogDashboardFileCreater.php
@@ -43,13 +43,13 @@ class CatalogDashboardFileCreater extends Command
         $sources = ['IN', 'US'];
         $record_arrays = [];
         $dbname = config('database.connections.buybox.database');
-        foreach($sources as $source){
-            
-            $asin_priority = [1 => 0,2 => 0,3 => 0];
+        foreach ($sources as $source) {
+
+            $asin_priority = [1 => 0, 2 => 0, 3 => 0];
             $catalog = [1 => 0, 2 => 0, 3 => 0];
-            $asin_delist = [1 => 0,2 => 0,3 => 0];
-            $asin_bb_price = [1 => 0,2 => 0,3 => 0];
-            $cat_price = [1 => 0,2 => 0,3 => 0];
+            $asin_delist = [1 => 0, 2 => 0, 3 => 0];
+            $asin_bb_price = [1 => 0, 2 => 0, 3 => 0];
+            $cat_price = [1 => 0, 2 => 0, 3 => 0];
 
             $source = strtolower($source);
             $destination_table = "asin_destination_${source}s";
@@ -58,77 +58,74 @@ class CatalogDashboardFileCreater extends Command
             $catalog_price = "pricing_${source}s";
 
             $priority_wise = DB::connection('catalog')
-            ->select("SELECT count(asin) as priority_wise, priority from ${destination_table} 
+                ->select("SELECT count(asin) as priority_wise, priority from ${destination_table} 
             group by priority");
-            foreach($priority_wise as $priority)
-            {
+            foreach ($priority_wise as $priority) {
                 $value = $priority->priority;
                 $asin_priority[$value] = $priority->priority_wise;
             }
-            
+
             $Total_catalogs = DB::connection('catalog')
-            ->select("SELECT count(${destination_table}.asin) as asin_catalog ,${destination_table}.priority from ${destination_table}
+                ->select("SELECT count(${destination_table}.asin) as asin_catalog ,${destination_table}.priority from ${destination_table}
             join ${catalog_table}
             ON ${destination_table}.asin = ${catalog_table}.asin
             group by ${destination_table}.priority
             ");
-            
-            foreach($Total_catalogs as $total_catalog)
-            {
+
+            foreach ($Total_catalogs as $total_catalog) {
                 $cat = $total_catalog->priority;
                 $catalog[$cat] = $total_catalog->asin_catalog;
             }
 
             $delist_asins = DB::connection('catalog')
-            ->select("SELECT count(${destination_table}.asin) as asin_delist, ${destination_table}.priority from ${destination_table}
+                ->select("SELECT count(${destination_table}.asin) as asin_delist, ${destination_table}.priority from ${destination_table}
             join ${dbname}.${buybox_table}
             ON ${destination_table}.asin = ${buybox_table}.asin
             WHERE ${buybox_table}.delist = 1
             group by ${destination_table}.priority
             ");
 
-            foreach($delist_asins as $delist_asin){
+            foreach ($delist_asins as $delist_asin) {
                 $delist = $delist_asin->priority;
                 $asin_delist[$delist] = $delist_asin->asin_delist;
             }
-            
+
             $bb_prices =  DB::connection('catalog')
-            ->select("SELECT count(${destination_table}.asin) as catalog_price, ${destination_table}.priority from ${destination_table}
+                ->select("SELECT count(${destination_table}.asin) as catalog_price, ${destination_table}.priority from ${destination_table}
             join ${dbname}.${buybox_table}
             ON ${destination_table}.asin = ${buybox_table}.asin
             group by ${destination_table}.priority
             ");
-            foreach($bb_prices as $bb_price){
+            foreach ($bb_prices as $bb_price) {
                 $price = $bb_price->priority;
                 $asin_bb_price[$price] = $bb_price->catalog_price;
             }
 
             $cat_pricings = DB::connection('catalog')
-            ->select("SELECT count(${destination_table}.asin) as price, ${destination_table}.priority from ${destination_table}
+                ->select("SELECT count(${destination_table}.asin) as price, ${destination_table}.priority from ${destination_table}
             join ${catalog_price}
             ON ${destination_table}.asin = ${catalog_price}.asin
             group by ${destination_table}.priority
             ");
-            foreach($cat_pricings as $cat_pricing){
+            foreach ($cat_pricings as $cat_pricing) {
                 $pr_priority = $cat_pricing->priority;
                 $cat_price[$pr_priority] = $cat_pricing->price;
             }
 
-            $record_arrays []  = [
-            'priority_wise_asin' => $asin_priority,
-            'catalog' => $catalog,
-            'delist_asin' => $asin_delist,
-            'bb_price' => $asin_bb_price,
-            'catalog_price' => $cat_price,
+            $record_arrays[]  = [
+                'priority_wise_asin' => $asin_priority,
+                'catalog' => $catalog,
+                'delist_asin' => $asin_delist,
+                'bb_price' => $asin_bb_price,
+                'catalog_price' => $cat_price,
             ];
-
         }
+
         // $cat_dashboard_file = "excel/downloads/catalog-dashboard-file.json";
         $cat_dashboard_file = "Dashboard/catalog-dashboard-file.json";
-        if(!Storage::exists($cat_dashboard_file)){
+        if (!Storage::exists($cat_dashboard_file)) {
             Storage::put($cat_dashboard_file, '');
         }
         Storage::put($cat_dashboard_file, json_encode($record_arrays));
-        Log::warning("Catalog dashboard file create successfully!");
     }
 }
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -32,13 +32,14 @@ class Kernel extends ConsoleKernel
             $schedule->command('backup:clean')->daily()->at('01:00');
             $schedule->command('pms:boe-upload-Do')->everyFourHours()->thenPing('http://beats.envoyer.io/heartbeat/uoR2oSENfKrIC4z');
             $schedule->command('pms:remove-uploaded-boe')->daily()->thenPing('http://beats.envoyer.io/heartbeat/uoR2oSENfKrIC4z');
-
             $schedule->command('pms:sellers-orders-import')->everyThirtyMinutes()->thenPing('http://beats.envoyer.io/heartbeat/uoR2oSENfKrIC4z');
-            //$schedule->command('mosh:seller-asin-get-pricing')->daily()->thenPing('http://beats.envoyer.io/heartbeat/uoR2oSENfKrIC4z');
+
             $schedule->command('mosh:catalog-amazon-import')->everyFiveMinutes()->thenPing('http://beats.envoyer.io/heartbeat/uoR2oSENfKrIC4z');
             $schedule->command('mosh:Catalog-price-import-bb-in')->everyMinute()->thenPing('http://beats.envoyer.io/heartbeat/uoR2oSENfKrIC4z');
             $schedule->command('mosh:Catalog-price-import-bb-us')->everyMinute()->thenPing('http://beats.envoyer.io/heartbeat/uoR2oSENfKrIC4z');
-            $schedule->command('mosh:catalog-dashboard-file')->everyFiveMinutes()->thenPing('http://beats.envoyer.io/heartbeat/uoR2oSENfKrIC4z');
+            $schedule->command('mosh:catalog-dashboard-file')->everyTenMinutes()->thenPing('http://beats.envoyer.io/heartbeat/uoR2oSENfKrIC4z');
+
+            //$schedule->command('mosh:seller-asin-get-pricing')->daily()->thenPing('http://beats.envoyer.io/heartbeat/uoR2oSENfKrIC4z');
         }
 
         if (app()->environment() === 'staging') {
