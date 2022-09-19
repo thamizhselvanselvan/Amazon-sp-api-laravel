b6f0249d24a113212f47a32856092f5154ef7d65
amitpms <nu.palmate@gmail.com>
2022-09-19 11:13:08 +0530

    rate chart updated

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -305,155 +305,27 @@ class TestController extends Controller
 
   public function INDToSA($weight, $bb_price)
   {
-    $rate_array = $this->rate_master_in_sa;
-    $int_shipping_base_charge = '';
-    foreach ($rate_array as $key => $value) {
-
-      if ($key >= $weight) {
-        $int_shipping_base_charge = $value['lmd_cost'];
-        break;
-      }
-    }
-
-    $duty_rate = 7 / 100;
-    $nitshopp = 12.0 / 100;
-    $packaging = 100.00;
-    $amazon_commission = 15.0 / 100;
-    $ex_rate = 0.051;
-
-    $duty_cost = ($bb_price + $int_shipping_base_charge) * $duty_rate;
-
-    $price_befor_amazon_fees = ($bb_price + $int_shipping_base_charge + $duty_cost + $packaging) +
-      (($bb_price + $int_shipping_base_charge + $duty_cost + $packaging) * $nitshopp);
-
-    $mbm_usd_sp = $price_befor_amazon_fees * (1 + $amazon_commission) +
-      ($amazon_commission * $price_befor_amazon_fees * 0.14);
-
-    $uae_sa = $mbm_usd_sp * $ex_rate;
-
-    return round($uae_sa, 2);
+    return (new PriceConversion())->INDToSA($weight, $bb_price);
   }
 
   public function INDToSG($weight, $bb_price)
   {
-    //India to Singapore
-    $rate_array = $this->rate_master_in_sg;
-    $int_shipping_base_charge = '';
-    foreach ($rate_array as $key => $value) {
-
-      if ($key >= $weight) {
-        $int_shipping_base_charge = $value['lmd_cost'];
-        break;
-      }
-    }
-
-    $duty_rate = 7 / 100;
-    $nitshopp = 15.0 / 100;
-    $packaging = 120.00;
-    $amazon_commission = 15.0 / 100;
-    $ex_rate = 0.019;
-
-    $duty_cost = ($bb_price + $int_shipping_base_charge) * $duty_rate;
-
-    $price_befor_amazon_fees = ($bb_price + $int_shipping_base_charge + $duty_cost + $packaging) +
-      (($bb_price + $int_shipping_base_charge + $duty_cost + $packaging) * $nitshopp);
-
-    $mbm_usd_sp = $price_befor_amazon_fees * (1 + $amazon_commission) +
-      ($amazon_commission * $price_befor_amazon_fees * 0.14);
-
-    $uae_sg = $mbm_usd_sp * $ex_rate;
-
-    return round($uae_sg, 2);
+    return (new PriceConversion())->INDToSG($weight, $bb_price);
   }
 
   public function INDToUAE($weight, $bb_price)
   {
-    $rate_array = $this->rate_master_in_ae;
-    $int_shipping_base_charge = '';
-
-    foreach ($rate_array as $key => $value) {
-
-      if ($key >= $weight) {
-        $int_shipping_base_charge = $value['lmd_cost'];
-        break;
-      }
-    }
-    $duty_rate = 7 / 100;
-    $nitshopp = 12.0 / 100;
-    $packaging = 180.00;
-    $amazon_commission = 15.0 / 100;
-    $ex_rate = 0.051;
-
-    $duty_cost = ($bb_price + $int_shipping_base_charge) * $duty_rate;
-
-    $price_befor_amazon_fees = ($bb_price + $int_shipping_base_charge + $duty_cost + $packaging) +
-      (($bb_price + $int_shipping_base_charge + $duty_cost + $packaging) * $nitshopp);
-
-    $mbm_usd_sp = $price_befor_amazon_fees * (1 + $amazon_commission) +
-      ($amazon_commission * $price_befor_amazon_fees * 0.14);
-
-    $uae_sp = $mbm_usd_sp * $ex_rate;
-
-    return round($uae_sp, 2);
+    return (new PriceConversion())->INDToUAE($weight, $bb_price);
   }
 
   public function USAToINDb2c($weight, $bb_price)
   {
-
-    if ($weight > 0.9) {
-      $int_shipping_base_charge = (7.5 + ($weight - 1) * 4.5);
-    } else {
-      $int_shipping_base_charge = 4.5;
-    }
-
-    // return $int_shipping_base_charge;
-    $duty_rate = 56.00 / 100;
-    $packaging = 2;
-    $seller_commission = ($bb_price + $int_shipping_base_charge + $packaging) * (15 / 100);
-    // $amazon_commission = 22.00 / 100;
-
-    // return $seller_commission;
-    $price_before_duty = $bb_price + $int_shipping_base_charge + $packaging + $seller_commission;
-    $ex_rate = 82;
-    $duty_cost = ($price_before_duty * $duty_rate);
-
-    // return $duty_cost;
-
-
-    $usd_sp = ($price_before_duty + $duty_cost) + ($price_before_duty + $duty_cost) * (20 / 100);
-
-    $india_sp = $usd_sp * $ex_rate;
-    return round($india_sp, 2);
+    return (new PriceConversion())->USAToINDB2C($weight, $bb_price);
   }
 
   public function USAToINDb2b($weight, $bb_price)
   {
-
-    if ($weight > 0.9) {
-      $int_shipping_base_charge = (6 + ($weight - 1) * 6);
-    } else {
-      $int_shipping_base_charge = 6;
-    }
-
-    $duty_rate = 32.00 / 100;
-    $seller_commission = 10 / 100;
-    $packaging = 2;
-    $amazon_commission = 22.00 / 100;
-
-    $ex_rate = 82;
-    $duty_cost = ($duty_rate * ($bb_price + $int_shipping_base_charge));
-
-    $price_befor_amazon_fees = ($bb_price + $int_shipping_base_charge + $duty_cost + $packaging) +
-      (($bb_price + $int_shipping_base_charge + $duty_cost + $packaging) * $seller_commission);
-
-    $usd_sp = $price_befor_amazon_fees * (1 + $amazon_commission) +
-      ($amazon_commission * $price_befor_amazon_fees * 0.12);
-
-    $india_sp = $usd_sp * $ex_rate;
-    return round($india_sp, 2);
-
-
-    //
+    return (new PriceConversion())->USAToINDB2B($weight, $bb_price);
   }
 
   public function getWeight($dimensions)
@@ -473,64 +345,17 @@ class TestController extends Controller
 
   public function USATOUAE($weight, $bb_price)
   {
-    $duty_rate = 5 / 100;
-    $seller_commission = 10 / 100;
-    $packaging = 4;
-    $amazon_commission = 15.00 / 100;
-    $int_shipping_base_charge = $weight * 4.5;
-    $ex_rate = 3.7;
-    $duty_cost = ($duty_rate * ($bb_price + $int_shipping_base_charge));
-
-    $price_befor_amazon_fees = ($bb_price + $int_shipping_base_charge + $duty_cost + $packaging) +
-      (($bb_price + $int_shipping_base_charge + $duty_cost + $packaging) * $seller_commission);
-
-    $usd_sp = $price_befor_amazon_fees * (1 + $amazon_commission) +
-      ($amazon_commission * $price_befor_amazon_fees * 0.12);
-
-    $IED_sp = $usd_sp * $ex_rate;
-    return round($IED_sp, 2);
+    return (new PriceConversion())->USATOUAE($weight, $bb_price);
   }
 
   public function USATOSG($weight, $bb_price)
   {
-
-    if ($weight > 0.9) {
-      $int_shipping_base_charge = (8 + ($weight - 1) * 4.5);
-    } else {
-      $int_shipping_base_charge = 8;
-    }
-
-    // return $int_shipping_base_charge;
-    $duty_rate = 4.00 / 100;
-    $seller_commission = 10 / 100;
-    $packaging = 3;
-    $MBM = 10.0 / 100;
-    $amazon_commission = 12.00 / 100;
-
-    $ex_rate = 1.37;
-    $duty_cost = $duty_rate * $bb_price;
-
-
-    $price_befor_amazon_fees = ($bb_price + $int_shipping_base_charge + $duty_cost + $packaging) +
-      (($bb_price + $int_shipping_base_charge + $duty_cost + $packaging) * $MBM);
-
-    $mbm_usd_sp = $price_befor_amazon_fees * (1 + $amazon_commission) +
-      ($amazon_commission * $price_befor_amazon_fees * 0.14);
-
-    // return $mbm_usd_sp;
-    $sg_sp = $mbm_usd_sp * $ex_rate;
-
-    return round($sg_sp, 2);
-    //
+    return (new PriceConversion())->USATOSG($weight, $bb_price);
   }
 
   public function INToSA($weight, $bb_price)
   {
-
-    $priceConverter = new PriceConversion();
-
-    // return $priceConverter->USAToIND($weight, $bb_price);
-    //
+    return (new PriceConversion())->INDToSA($weight, $bb_price);
   }
 
   public function ExportCatalog()
