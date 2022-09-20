af321bd421a82704d7c9fde61e0739efb4105301
amitpms <nu.palmate@gmail.com>
2022-09-20 10:56:56 +0530

    supervisor added for nitrous, mbm and mahzuz stores

--- app/Services/SP_API/API/Order/OrderItem.php
+++ app/Services/SP_API/API/Order/OrderItem.php
@@ -50,21 +50,21 @@ class OrderItem
             $this->OrderItemDataFormating($result_orderItems, $result_order_address, $order_id, $awsCountryCode, $aws_id);
         } catch (Exception $e) {
 
-           // Log::warning($e->getMessage());
+            // Log::warning($e->getMessage());
             $code =  $e->getCode();
             $msg = $e->getMessage();
             $error_reportings = ErrorReporting::create([
-            'queue_type' => "order",
-            'identifier' => $order_id,
-            'identifier_type' => "order_id",
-            'source' => $awsCountryCode,
-            'aws_key' => $aws_id,
-            'error_code' => $code,
-            'message' => $msg,
+                'queue_type' => "order",
+                'identifier' => $order_id,
+                'identifier_type' => "order_id",
+                'source' => $awsCountryCode,
+                'aws_key' => $aws_id,
+                'error_code' => $code,
+                'message' => $msg,
             ]);
         }
         return true;
-            }
+    }
 
     public function OrderItemDataFormating($result_orderItems, $result_order_address, $order_id, $awsCountryCode, $aws_id)
     {
