f9b8d5efc67205f68d81e8dae1aea920051c7c7e
vikesh <zeta.palmate@gmail.com>
2022-09-27 10:38:38 +0530

    move catalog search asin source module to mosh amazon catalog module.

--- resources/views/Catalog/product/index.blade.php
+++ resources/views/Catalog/product/index.blade.php
@@ -154,6 +154,8 @@
                 </div>
             </div>
         </div>
+        
+
     </div>
     @stop
 
@@ -188,94 +190,188 @@
             </div>
         </div>
     </div>
+    <div class="row">
+        <div class="col-2"></div>
+        <div class="col-8 ">
+            <div class="card ">
+                <div class="card-body ">
+                    <label for="Select Source" class ="mt-0">Select Source</label><br>
+                    <div class="row ">
+                        <div class="col-1">
+                            <input type="radio" class="Asin-source" name="source" value="IN"  />
+                            <label for="IN">IN</label>
+                        </div>
+                        <div class="col-1">
+                            <input type="radio" class="Asin-source" name="source" value="US"  />
+                            <label for="US">US</label>
+                        </div>
+                        
+                    </div>
+                    <x-adminlte-textarea label="Search Catalog" type="text-area" class="Asins" name="catalog_asins" placeholder="Enter Asin" rows="4" />
+                    <b><p class="text-danger" id="error"></p></b>
+                    <x-adminlte-button label="Search" type="submit" theme="primary" icon="fas fa-search text-danger" class="search-catalog btn-sm float-right mt-2 " />
+                </div>
+            </div>
+        </div>
+        <div class="col-2"></div>
+    </div>
+
+    <div class="row d-none display-data">
+        <table class="table table-bordered yajra-datatable table-striped text-center table-sm ">
+            <thead class="">
+                <tr class="bg-info thead"></tr>
+                
+            </thead>
+            <tbody class="search-data">
+            </tbody>
+        </table>
+    </div>
 
     @stop
 
     @section('js')
     <script type="text/javascript">
-    $('#country').on('change', function() {
-        let country_code = $(this).val();
-        if (country_code != 'NULL') {
-            yajraTable(country_code);
-        }
-    });
-
-    $(document).ready(function() {
-        $('#country').change(function() {
-            document.getElementById('countrymsg').innerHTML = '';
+        $('#country').on('change', function() {
+            let country_code = $(this).val();
+            if (country_code != 'NULL') {
+                yajraTable(country_code);
+            }
         });
-    });
 
-    $('#catalogdownload').click(function() {
-        
-        $.ajax({
-            url: "/catalog/get-file",
-            method: "GET",
-            data: {
-                "catalog": "catalog",
-                "_token": "{{ csrf_token() }}",
-            },
-            success: function(response) {
-                if(response == ''){
-                    $('.catalogFiles').append('File Downloading..');
-                }else{
-
-                    $('.catalogFiles').empty();
-                    console.log(response);
-                    let files = '';
-                    $.each(response, function(index, response) {
-                        let data = response;
-                        $.each(data, function(key, data){
-                            
-                            files += "<li class='p-0 m-0'>";
-                            files += "<a href='/catalog/download/csv-file/" + index + "/"+ key +
-                                "' class='p-0 m-0'> Catalog " + index + '&nbsp;' + key +"</a> ";
-                            files += data;
-                            files += "</li>";
-                        });
-                        // let file_name = Object.keys(response)[0];
-                        // let file_time = response[file_name];
-                    });
-                    $('.catalogFiles').html(files);
-                }
-                
-            },
+        $(document).ready(function() {
+            $('#country').change(function() {
+                document.getElementById('countrymsg').innerHTML = '';
+            });
         });
-    });
 
-    $('#download_catalog_price').click(function() {
-        $.ajax({
-            url: "/catalog/get-file",
-            data: {
-                "method": "GET",
-                "catalog": "catalog_price",
-                "_token": "{{ csrf_token() }}",
-            },
-            success: function(result) {
-
-                if(result == ''){
-                    $('.catalogPricing').append('File Downloading..');
-                }else{
-
-                    $('.catalogPricing').empty();
-                    let files = '';
-                    $.each(result, function(index, result) {
-                        let data = result;
-                        $.each(data, function(key, data) {
-                            files += "<li class='p-0 m-0'>";
-                            files += "<a href='/catalog/download/price/" + index + '/' +
-                                key +
-                                "' class='p-0 m-0'> Catalog Price " + index + '&nbsp;' +
-                                key + "</a> ";
-                            files += data;
-                            files += "</li>";
-                        });
-                    });
-                    $('.catalogPricing').append(files);
-                }
-            },
+        $('#catalogdownload').click(function() {
             
+            $.ajax({
+                url: "/catalog/get-file",
+                method: "GET",
+                data: {
+                    "catalog": "catalog",
+                    "_token": "{{ csrf_token() }}",
+                },
+                success: function(response) {
+                    if(response == ''){
+                        $('.catalogFiles').append('File Downloading..');
+                    }else{
+
+                        $('.catalogFiles').empty();
+                        console.log(response);
+                        let files = '';
+                        $.each(response, function(index, response) {
+                            let data = response;
+                            $.each(data, function(key, data){
+                                
+                                files += "<li class='p-0 m-0'>";
+                                files += "<a href='/catalog/download/csv-file/" + index + "/"+ key +
+                                    "' class='p-0 m-0'> Catalog " + index + '&nbsp;' + key +"</a> ";
+                                files += data;
+                                files += "</li>";
+                            });
+                            // let file_name = Object.keys(response)[0];
+                            // let file_time = response[file_name];
+                        });
+                        $('.catalogFiles').html(files);
+                    }
+                    
+                },
+            });
         });
-    });
+
+        $('#download_catalog_price').click(function() {
+            $.ajax({
+                url: "/catalog/get-file",
+                data: {
+                    "method": "GET",
+                    "catalog": "catalog_price",
+                    "_token": "{{ csrf_token() }}",
+                },
+                success: function(result) {
+
+                    if(result == ''){
+                        $('.catalogPricing').append('File Downloading..');
+                    }else{
+
+                        $('.catalogPricing').empty();
+                        let files = '';
+                        $.each(result, function(index, result) {
+                            let data = result;
+                            $.each(data, function(key, data) {
+                                files += "<li class='p-0 m-0'>";
+                                files += "<a href='/catalog/download/price/" + index + '/' +
+                                    key +
+                                    "' class='p-0 m-0'> Catalog Price " + index + '&nbsp;' +
+                                    key + "</a> ";
+                                files += data;
+                                files += "</li>";
+                            });
+                        });
+                        $('.catalogPricing').append(files);
+                    }
+                },
+                
+            });
+        });
+
+        // START CATALOG BULK SEARCH
+
+        $('.search-catalog').on('click', function(){
+        
+            $('.display-data').addClass('d-block');
+            let catalog_asins = $('.Asins').val();
+            let source = $('input[name="source"]:checked').val();
+
+            if(!$('input[name="source"]:checked').val() ){
+                alert('Please choose source');
+                return false;
+            }
+            else if(catalog_asins == ''){
+                document.getElementById('error').innerHTML = 'Please enter Asin *';
+                $('.thead').empty();
+                $('.search-data').empty();
+
+                return false;
+            }else{
+                document.getElementById('error').innerHTML = '';
+                $.ajax({
+                    method: 'post',
+                    url: "{{ route('catalog.asin.search') }}",
+                    data: {
+                        "catalog_asins" : catalog_asins,
+                        "source": source,
+                        "_token": "{{ csrf_token() }}",
+                    },
+                    success: function(result) {
+                        let data = '';
+                        let head = "";
+                        let str_replace = '';
+                        $('.thead').empty();
+                        $.each(result[0], function(key, record){
+                            head += " <td>"+ key +"</td> ";
+                            str_replace = head.replace(/_+/g, ' ').toUpperCase() ;
+                            
+                        });
+                        $('.thead').append(str_replace);
+
+                        $.each(result, function(key1, record1){
+                            data += '<tr>';
+                            $.each(record1, function(key2, value){
+                                data += "<td>"+ value +"</td>"
+                            });
+                            data +='</tr>';
+                        });
+                        $('.search-data').html(data);
+                    },
+                    error: function(result) {
+                        // alert('Data not found!');
+                    }
+                });
+            }
+        });
+
+        // END CATALOG BULK SEARCH
     </script>
     @stop
