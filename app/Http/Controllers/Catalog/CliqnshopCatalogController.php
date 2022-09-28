6b9dff4a4a5ba8ef75c441f01773eea34f21b83d
sanju <epsilon.palmate@gmail.com>
2022-09-28 16:55:37 +0530

    Images column Addded

--- app/Http/Controllers/Catalog/CliqnshopCatalogController.php
+++ app/Http/Controllers/Catalog/CliqnshopCatalogController.php
@@ -32,7 +32,6 @@ class CliqnshopCatalogController extends Controller
             'Product Name',
             'short description',
             'long description',
-            'Images',
             'Price',
             'price quantity',
             'price tax rate',
@@ -41,7 +40,12 @@ class CliqnshopCatalogController extends Controller
             'Suggested Products',
             'Products bought together',
             'stock level',
-            'date of back in stock'
+            'date of back in stock',
+            'Images1',
+            'Images2',
+            'Images3',
+            'Images4',
+            'Images5'
         ];
         $exportFilePath = 'Cliqnshop/catalog.csv';
         if (!Storage::exists($exportFilePath)) {
@@ -50,30 +54,51 @@ class CliqnshopCatalogController extends Controller
         $writer = Writer::createFromPath(Storage::path($exportFilePath), "w");
         $writer->insertOne($headers);
 
-        foreach($cat_details as $data)
-        {
+        foreach ($cat_details as $data) {
+
+            $data = (array)$data;
+            $imagedata = json_decode($data['images'], true);
+
+            $img1 = null;
+            if(array_key_exists('images', $imagedata[0])) {
+
+                foreach($imagedata[0]['images'] as $counter => $image_data_new) {
+                    $counter++;
+                    if (array_key_exists("link", $image_data_new)) {
+                        $img1["Images${counter}"] = $image_data_new['link'];
+                    } 
+                    
+                    if($counter == 5){
+                        break;
+                    }
+                }
+            }
+            
+
             $csv_array = [
                 'Category' => null,
-              'Sub-Category' => null,
-            'Brand' =>$data->brand,
-            'ASIN' => $data->asin,
-            'Product Name' => $data->asin,
-            'short description' => null,
-            'long description' => null,
-            'Images' => $data->images,
-            'Price' => $data->inprice,
-            'price quantity' => '1',
-            'price tax rate' => '19',
-            'Attributese' => null,
-            'product variants' => null,
-            'Suggested Products' => null,
-            'Products bought together' => null,
-            'stock level' => '500',
-            'date of back in stock' => null
+                'Sub-Category' => null,
+                'Brand' => ucfirst($data['brand']),
+                'ASIN' => $data['asin'],
+                'Product Name' => $data['item_name'],
+                'short description' => null,
+                'long description' => null,
+                'Price' => $data['inprice'],
+                'price quantity' => '1',
+                'price tax rate' => '19',
+                'Attributese' => null,
+                'product variants' => null,
+                'Suggested Products' => null,
+                'Products bought together' => null,
+                'stock level' => '500',
+                'date of back in stock' => null
             ];
 
+            $csv_array = [...$csv_array, ...$img1];
+
+
+
             $writer->insertOne($csv_array);
-            
         }
         return redirect('/catalog/product')->with("success", "CliqnshopCatalog Price is Exporting please Wait for Few Minuts.");
     }
@@ -90,7 +115,7 @@ class CliqnshopCatalogController extends Controller
         $folder = $request->catalog;
         $path = Storage::path($folder);
         $files = scandir($path);
-       
+
         return response()->json($files);
     }
     public function DownloadCatalogcloqnshop()
@@ -98,7 +123,4 @@ class CliqnshopCatalogController extends Controller
         $path = "Cliqnshop/catalog.csv";
         return Storage::download($path);
     }
-
-
-    
 }
