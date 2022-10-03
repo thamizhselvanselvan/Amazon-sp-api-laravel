82b217f1a564d55ac74a285f158fad043389b77d
vikesh <zeta.palmate@gmail.com>
2022-10-03 10:13:16 +0530

    Fixed asin destination controller.
    Signed-off-by: vikesh <zeta.palmate@gmail.com>

--- resources/views/Catalog/AsinDestination/index.blade.php
+++ resources/views/Catalog/AsinDestination/index.blade.php
@@ -3,238 +3,182 @@
 @section('title', 'ASIN Destination')
 
 @section('css')
-<link rel="stylesheet" href="/css/styles.css">
+    <link rel="stylesheet" href="/css/styles.css">
 @stop
 
 @section('content_header')
-<div class="row">
-    <div class="col">
+    <div class="row">
+        <div class="col">
 
-        <h1 class="m-0 text-dark"> ASIN Destination </h1>
-    </div>
-    <div class="col ">
+            <h1 class="m-0 text-dark"> ASIN Destination </h1>
+        </div>
+        <div class="col ">
 
-        <h2 class="mb-4 float-right">
+            <h2 class="mb-4 float-right">
 
-            <a href="import-asin-destination">
-                <x-adminlte-button label="Asin Bulk Import" theme="primary" icon="fas fa-file-import" class="btn-sm" />
-            </a>
+                <a href="import-asin-destination">
+                    <x-adminlte-button label="Asin Bulk Import" theme="primary" icon="fas fa-file-import" class="btn-sm" />
+                </a>
 
-            <!-- <a href="{{route('catalog.asin.dastination.export')}}">
-                <x-adminlte-button label="Asin Export" theme="primary" icon="fas fa-file-export" class="btn-sm" />
-            </a>
+                <a href="{{ route('catalog.destination.download.template') }}">
+                    <x-adminlte-button label="Download Template" theme="primary" icon="fas fa-file-download"
+                        id="exportUniversalTextiles" class="btn-sm" />
+                </a>
 
-            <x-adminlte-button label="Download Asin" theme="primary" icon="fas fa-file-download" data-toggle="modal"
-                data-target="#exampleModal" class="btn-sm"></x-adminlte-button> -->
+                <x-adminlte-button label="Asin Truncate" theme="primary" icon="fas fa-trash text-danger" class="btn-sm"
+                    data-toggle="modal" data-target="#destinationTruncate"></x-adminlte-button>
 
-            <a href="{{ route('catalog.destination.download.template') }}">
-                <x-adminlte-button label="Download Template" theme="primary" icon="fas fa-file-download"
-                    id="exportUniversalTextiles" class="btn-sm" />
-            </a>
+                <div class="modal fade" id="destinationTruncate" tabindex="-1" role="dialog"
+                    aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">ASIN Table Truncate</h5>
+                                <button type="button" class="close btn-sm" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body" style="font-size:15px">
+                                <form action="{{ route('catalog.asin.destination.truncate') }}">
+                                    <h5>Select Destination</h5>
+                                    <div class="row ">
+                                        <div class="col-2">
+                                            <label for="AE">AE</label>
+                                            <input type="checkbox" name="destination[]" value="AE">
+                                        </div>
+                                        <div class="col-2">
+                                            <label for="IN">IN</label>
+                                            <input type="checkbox" name="destination[]" value="IN">
+                                        </div>
+                                        <div class="col-2">
+                                            <label for="US">US</label>
+                                            <input type="checkbox" name="destination[]" value="US">
+                                        </div>
+                                    </div><br>
 
-    
-            <x-adminlte-button label="Asin Truncate" theme="primary" icon="fas fa-trash text-danger"
-                 class="btn-sm" data-toggle="modal" data-target="#destinationTruncate"></x-adminlte-button>
-            
-            <div class="modal fade" id="destinationTruncate" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-                aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">ASIN Table Truncate</h5>
-                            <button type="button" class="close btn-sm" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body" style="font-size:15px">
-                            <form action="{{ route('catalog.asin.destination.truncate') }}">
-                                <h5>Select Destination</h5>
-                                <div class="row ">
-                                    <div class="col-2">
-                                        <label for="AE">AE</label>
-                                        <input type="checkbox" name="destination[]" value="AE" >
+                                    <h5>Select Priority</h5>
+                                    <div class="row ">
+                                        <div class="col-2">
+                                            <label for="P1">P1</label>
+                                            <input type="radio" class="destination-priority" name="priority"
+                                                value="1">
+                                        </div>
+                                        <div class="col-2">
+                                            <label for="P2">P2</label>
+                                            <input type="radio" class="destination-priority" name="priority"
+                                                value="2">
+                                        </div>
+                                        <div class="col-2 ">
+                                            <label for="P3">P3</label>
+                                            <input type="radio" class="destination-priority" name="priority"
+                                                value="3">
+                                        </div>
                                     </div>
-                                    <div class="col-2">
-                                        <label for="IN">IN</label>
-                                        <input type="checkbox" name="destination[]" value="IN" >
+                                    <div class="col-12 float-left mt-2">
+                                        <x-adminlte-button label="Truncate" theme="danger" class="btn btn-sm truncate"
+                                            icon="fas fa-trash " type="submit" />
                                     </div>
-                                    <div class="col-2">
-                                        <label for="US">US</label>
-                                        <input type="checkbox" name="destination[]" value="US" >
-                                    </div>
-                                </div><br>
+                                </form>
 
-                                <h5>Select Priority</h5>
-                                <div class="row ">
-                                    <div class="col-2">
-                                        <label for="P1">P1</label>
-                                        <input type="radio" class="destination-priority" name="priority" value="1"  >
-                                    </div>
-                                    <div class="col-2">
-                                        <label for="P2">P2</label>
-                                        <input type="radio" class="destination-priority" name="priority" value="2"  >
-                                    </div>
-                                    <div class="col-2 ">
-                                        <label for="P3">P3</label>
-                                        <input type="radio" class="destination-priority" name="priority" value="3"  >
-                                    </div>
-                                </div>
-                                <div class="col-12 float-left mt-2">
-                                    <x-adminlte-button label="Truncate" theme="danger" class="btn btn-sm truncate" icon="fas fa-trash " type="submit" />
-                                </div>
-                            </form>
-
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary btn-danger" data-dismiss="modal"  >Close</button>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary btn-danger"
+                                    data-dismiss="modal">Close</button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-
-            <!-- <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-                aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Asin Download</h5>
-                            <button type="button" class="close btn-sm" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-
-                            <a href="{{ route('catalog.download.asin.destination') }}">
-                                <h6>Download ASIN </h6>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        </div>
-                    </div>
-                </div>
-            </div> -->
-            </a>
-            <!-- <a href="{{ route('catalog.asin.destination.bin') }}">
-                <x-adminlte-button label="Bin" theme="primary" icon="fas fa-trash" class="btn-sm" />
-            </a> -->
-        </h2>
+            </h2>
+        </div>
     </div>
-</div>
 @stop
 
 @section('content')
 
-<div class="row">
-    <div class="col">
-        
-        <div class="alert_display">
-            @if ($message = Session::get('success'))
-            <div class="alert alert-success alert-block">
-                <button type="button" class="close" data-dismiss="alert">×</button>
-                <strong>{{ $message }}</strong>
+    <div class="row">
+        <div class="col">
+
+            <div class="alert_display">
+                @if ($message = Session::get('success'))
+                    <div class="alert alert-success alert-block">
+                        <button type="button" class="close" data-dismiss="alert">×</button>
+                        <strong>{{ $message }}</strong>
+                    </div>
+                @endif
             </div>
-            @endif
         </div>
     </div>
-</div>
-<div class="row">
-    <div class="col-2"></div>
-    <div class="col-8 ">
-        <form action=" {{ route('catalog.asin.destination.search.delete') }} " method="POST" >
-            @csrf
-            
-            <div class="card ">
-                <div class="card-header text-center mt-0 pt-0 mb-0 pb-0">
-                    <h3>Search ASIN And Delete</h3>
-                </div>
-                <div class="card-body ">
-                    <label for="Select Source" class="mt-0">Select Source</label><br>
-                    <div class="row ">
-                        <div class="col-1">
-                            <input type="radio" class="Asin-source" name="source" value="IN" />
-                            <label for="IN">IN</label>
-                        </div>
-                        <div class="col-1">
-                            <input type="radio" class="Asin-source" name="source" value="US" />
-                            <label for="US">US</label>
-                        </div>
-    
+    <div class="row">
+        <div class="col-2"></div>
+        <div class="col-8 ">
+            <form action=" {{ route('catalog.asin.destination.search.delete') }} " method="POST">
+                @csrf
+
+                <div class="card ">
+                    <div class="card-header text-center mt-0 pt-0 mb-0 pb-0">
+                        <h3>Search ASIN And Delete</h3>
                     </div>
-                    <label for="Select Source" class="mt-0">Select Priority</label><br>
-                    <div class="row ">
-                        <div class="col-1">
-                            <input type="radio" class="destination-priority" name="priority" value="1"  >
-                            <label for="P1">P1</label>
+                    <div class="card-body ">
+                        <label for="Select Source" class="mt-0">Select Source</label><br>
+                        <div class="row ">
+                            <div class="col-1">
+                                <input type="radio" class="Asin-source" name="source" value="IN" />
+                                <label for="IN">IN</label>
+                            </div>
+                            <div class="col-1">
+                                <input type="radio" class="Asin-source" name="source" value="US" />
+                                <label for="US">US</label>
+                            </div>
+
                         </div>
-                        <div class="col-1">
-                            <input type="radio" class="destination-priority" name="priority" value="2"  >
-                            <label for="P2">P2</label>
+                        <label for="Select Source" class="mt-0">Select Priority</label><br>
+                        <div class="row ">
+                            <div class="col-1">
+                                <input type="radio" class="destination-priority" name="priority" value="1">
+                                <label for="P1">P1</label>
+                            </div>
+                            <div class="col-1">
+                                <input type="radio" class="destination-priority" name="priority" value="2">
+                                <label for="P2">P2</label>
+                            </div>
+                            <div class="col-1 ">
+                                <input type="radio" class="destination-priority" name="priority" value="3">
+                                <label for="P3">P3</label>
+                            </div>
                         </div>
-                        <div class="col-1 ">
-                            <input type="radio" class="destination-priority" name="priority" value="3"  >
-                            <label for="P3">P3</label>
-                        </div>
+                        <x-adminlte-textarea label="Enter ASIN" type="text-area" class="Asins" name="Asins"
+                            placeholder="Enter Asin" rows="4" />
+                        <b>
+                            <p class="text-danger" id="error"></p>
+                        </b>
+                        <x-adminlte-button label="Search & Delete" type="submit" theme="primary"
+                            icon="fas fa-trash text-danger" class="search&delete btn-sm float-right mt-2 "
+                            onclick="return confirm('Are you sure you want to delete these asins?')" />
                     </div>
-                    <x-adminlte-textarea label="Enter ASIN" type="text-area" class="Asins" name="Asins" placeholder="Enter Asin" rows="4" />
-                    <b>
-                        <p class="text-danger" id="error"></p>
-                    </b>
-                    <x-adminlte-button label="Search & Delete" type="submit" theme="primary" icon="fas fa-trash text-danger" class="search&delete btn-sm float-right mt-2 " onclick="return confirm('Are you sure you want to delete these asins?')" />
                 </div>
-            </div>
-        </form>
+            </form>
+        </div>
+        <div class="col-2"></div>
     </div>
-    <div class="col-2"></div>
-</div>
 @stop
 
 @section('js')
-<script type="text/javascript">
+    <script type="text/javascript">
+        $(document).on('click', '.trash', function() {
+            let bool = confirm('Are you sure you want to delete?');
+            if (!bool) {
+                return false;
+            }
 
-$(document).on('click', '.trash', function() {
-    let bool = confirm('Are you sure you want to delete?');
-    if (!bool) {
-        return false;
-    }
+        });
 
-});
+        $(document).on('click', '.truncate', function() {
 
-$(document).on('click', '.truncate', function() {
+            let bool = confirm('Are you sure you want to truncate this selected table ?');
+            if (!bool) {
+                return false;
+            }
 
-    let bool = confirm('Are you sure you want to truncate this selected table ?');
-    if (!bool) {
-        return false;
-    }
-
-});
-
-// $(document).on('click', ".delete", function(e) {
-//     e.preventDefault();
-//     let bool = confirm('Are you sure you want to push this asin to Bin?');
-
-//     if (!bool) {
-//         return false;
-//     }
-//     let self = $(this);
-//     let id = self.attr('data-id');
-
-//     self.prop('disable', true);
-
-//     $.ajax({
-//         method: 'post',
-//         url: '/catalog/remove/asin/' + id,
-//         data: {
-//             "_token": "{{ csrf_token() }}",
-//         },
-//         response: 'json',
-//         success: function(response) {
-//             alert('Delete successfully');
-//             // location.reload()
-//             yajra_datatable();
-//         },
-//     });
-
-// });
-
-</script>
+        });
+    </script>
 @stop
