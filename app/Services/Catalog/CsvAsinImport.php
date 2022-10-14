2470260c520ab732d1ee034ebce9546a1d70ad98
vikesh <zeta.palmate@gmail.com>
2022-10-14 15:48:32 +0530

    Fixed catalog asin import queue.

--- app/Services/Catalog/CsvAsinImport.php
+++ app/Services/Catalog/CsvAsinImport.php
@@ -9,17 +9,19 @@ class CsvAsinImport
 {
     public function AsinImport($records)
     {
+        log::notice('Enter csv asin import');
         $count = 0;
         $country_code = strtolower($records['source']);
         foreach ($records['ASIN'] as $record) {
+            log::notice('Enter inside foreach');
 
             $asin_details[] = [
                 'asin' => $record,
                 'user_id' => $records['user_id'],
             ];
 
-            if ($count == 2500) {
-
+            if ($count == 2000) {
+                log::notice('inserting data');
                 $model_name = table_model_create(country_code: $country_code, model: 'Asin_source', table_name: 'asin_source_');
                 $model_name->upsert($asin_details, ['user_asin_unique'], ['asin', 'user_id']);
                 $count = 0;
