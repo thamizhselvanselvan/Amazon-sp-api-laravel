40739cf84e7bda09617dd04e9c3d075140a43f3e
sanju <epsilon.palmate@gmail.com>
2022-10-27 17:32:32 +0530

    Made Calalog price export date optional

--- routes/pms/buisness_api.php
+++ routes/pms/buisness_api.php
@@ -48,127 +48,16 @@ Route::resource('business/orders', 'BuisnessAPI\OrdersController');
 
 
 
-// Route::get('aimeos/imp', function () {
+// Route::get('orders', function () {
+//     $asin  = ['B00A2JBMRE', 'B06WW6W4KW'];
+//     $name = ['Homedics SoundSleep White Noise Sound Machine', 'Homedics SoundSleep Recharged Alarm Clock'];
+//     $OfferID = ['HRZ3O4fiD2OI5oG5u9p242Sa0KJyixLtlPmy2xvFPsvB9flV0Flk8KWlDgqRCK6xY1MWoKokKRJFBhB%2BgALwA2x%2BZmKBf4ZOPiWbKoY5HYTIB%2FW0PEsqK%2FqJPZLQcWI%2BNRpjlYvI396cR9XNEr51Y6H5nZHqYvtH', 'OBuz6XT%2Fv5%2BGCXdqq3xUDzqEVkVuF0FkXkMVc0kWWRTCQgHOsFX%2BY%2FLe%2FOQXc8tPSP2Svq4y6JTWMqOHiSySvzzEdDQtLmJXp65IY%2FnFMzEjWrY3iwXlREw6F%2Bk%2FH1d0PD7udz%2FKMOGhY12CXVan6qiHFus143ZhqViBBOO9fz5FP5J1n6PZqkfzZL%2Fm%2Byg6'];
+//     $ApiCall = new Orders();
 
-//     $data = DB::connection('cliqnshop')->table('order')
+//     $sent[] = ['asin' => $asin , 'item_name' => $name , 'OfferID' => $OfferID];
 
-//         ->join('order_base_product as oid', function ($query) {
-//             $query->on('oid.baseid', '=', 'order.baseid');
-//         })
-//         ->join('product as pid', function ($query) {
-//             $query->on('pid.id', '=', 'oid.prodid');
-//         })
-//         ->select('code', 'label')
-//         ->get();
-
-//         $call = new Orders;
-//       foreach ($data as $val) 
-//     {
-//         // po($data);
-//         $asin = ($val->code);
-//         $item_name  = ($val->label);
-
-
-//         $data =  $call->getOrders($asin, $item_name);
-//         $resultxml = $data[2];
-//         Storage::disk('local')->put('xml.txt', $resultxml);
-
-
-
-//         $responce = ($data[0]);
-
-//         $parse = simplexml_load_string($responce);
-//         $xmlr =  json_decode(json_encode($parse), true);
-//         $details = ($data[1]);
-//         $responce_code = ($xmlr["Response"]["Status"]["@attributes"]["code"]);
-//         $responce_text = ($xmlr["Response"]["Status"]["@attributes"]["text"]);
-//         $receved_payload = ($xmlr["@attributes"]["payloadID"]);
-
-//         $xml = ($data[2]);
-
-//         $order_details_array = ($data[1]);
-//         $order_details = ($order_details_array[0]);
-//         $sent_payload = ($order_details["payload"]);
-//         $order_date = ($order_details["order_date"]);
-//         $org_name =  ($order_details["organization_name"]);
-//         $name =  ($order_details["name"]);
-//         $email = ($order_details["e_mail"]);
-//         $countrycode = ($order_details["country_code"]);
-//         $country_name = ($order_details["country_name"]);
-//         $order_id = ($order_details["order_id"]);
-
-//         $deliver1 =   ($order_details["delivery_1"]);
-//         $deliver2 =   ($order_details["delivery_2"]);
-//         $deliver3 =   ($order_details["delivery_3"]);
-//         $street = ($order_details["street"]);
-//         $city = ($order_details["city"]);
-//         $state = ($order_details["state"]);
-//         $post_code = ($order_details["post_code"]);
-//         $area_code = ($order_details["area_code"]);
-//         $phone_no  = ($order_details["phone_no"]);
-//         $fax_name  = ($order_details["fax_name"]);
-
-//         $asin = ($order_details["asin"]);
-//         $item_description = ($order_details["item_description"]);
-//         $unit = ($order_details["unit"]);
-//         $class = ($order_details["class"]);
-//         $quantity = ($order_details["quantity"]);
-//         $ManufacturerName = ($order_details["ManufacturerName"]);
-//         $line = ($order_details["line"]);
-//         $ManufacturerPartID = ($order_details["ManufacturerPartID"]);
-//         $category = ($order_details["category"]);
-//         $sub_category = ($order_details["sub_category"]);
-
-//         $item_details = [
-//             $asin,
-//             $item_description,
-//             $unit,
-//             $class,
-//             $quantity,
-//             $ManufacturerName,
-//             $line,
-//             $ManufacturerPartID,
-//             $category,
-//             $sub_category,
-
-//         ];
-
-//         $ship_address_array = [
-//             $deliver1,
-//             $deliver2,
-//             $deliver3,
-//             $street,
-//             $city,
-//             $state,
-//             $post_code,
-//             $area_code,
-//             $phone_no,
-//             $fax_name,
-//         ];
-
-//         DB::connection('business')->table('orders')->insert([
-//             // 'xml_sent' => json_encode($xml),
-//             'xml_sent' => '',
-//             'sent_payload' => $sent_payload,
-//             'organization_name' => $org_name,
-//             'order_date' => $order_date,
-//             'name' => $name,
-//             'e-mail' => $email,
-//             'country_name' => $country_name,
-//             'country_code' => $countrycode,
-//             'order_id' => $order_id,
-//             'item_details' => json_encode($item_details),
-//             'ship_address' => json_encode($ship_address_array),
-//             'bill_address' => json_encode($ship_address_array),
-//             'responce_payload' => $receved_payload,
-//             'responce_text' =>  $responce_text,
-//             'responce_code' => $responce_code,
-//             'created_at' => now(),
-//             'updated_at' => now()
-//         ]);
-//         // dd($responce, $details);
-//     }
-    
+//     // dd($sent['asin']);
+//     // exit;
+//     $data = $ApiCall->getOrders($sent);
+//     dd($data);
 // });
-  
-
