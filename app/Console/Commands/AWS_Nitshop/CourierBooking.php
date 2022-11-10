38cd92807faa13f67dd287a5eb9fd80076724233
amitpms <nu.palmate@gmail.com>
2022-11-10 14:50:16 +0530

    courier booking job error fixed

--- app/Console/Commands/AWS_Nitshop/CourierBooking.php
+++ app/Console/Commands/AWS_Nitshop/CourierBooking.php
@@ -55,7 +55,7 @@ class CourierBooking extends Command
                 'courier_class' => $courier_name
             ];
 
-            jobDispatchFunc('Courier_Booking/CourierBookingJob', $job_parameters);
+            jobDispatchFunc('Courier_Booking\CourierBookingJob', $job_parameters);
         }
     }
 }
