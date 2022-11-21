20c83cbe40cdfe2cf55636c28e607e3f54768d13
amitpms <nu.palmate@gmail.com>
2022-11-21 16:50:29 +0530

    file management added into invoice

--- resources/views/invoice/uploadCsv.blade.php
+++ resources/views/invoice/uploadCsv.blade.php
@@ -5,22 +5,22 @@
 
 @section('content_header')
 
-    {{-- <div class="row mt-0">
+{{-- <div class="row mt-0">
         <div class="col">
             <h1 class="m-0 text-dark text-left ">Invoice Management</h1>
         </div>
     </div> --}}
-    <div class="row">
-        <div class="col mt-2">
-            <a href="{{ route('invoice.index') }}" class="btn btn-primary btn-sm">
-                <i class="fas fa-long-arrow-alt-left"></i> Back
-            </a>
-        </div>
-        <div class="col">
-            <h1 class="mt-2 text-center">Invoice Import</h1>
-        </div>
-        <div class="col"></div>
+<div class="row">
+    <div class="col mt-2">
+        <a href="{{ route('invoice.index') }}" class="btn btn-primary btn-sm">
+            <i class="fas fa-long-arrow-alt-left"></i> Back
+        </a>
     </div>
+    <div class="col">
+        <h1 class="mt-2 text-center">Invoice Import</h1>
+    </div>
+    <div class="col"></div>
+</div>
 
 @stop
 
@@ -30,79 +30,91 @@
 
 @section('content')
 
-    <div class="loader d-none">
-        <div class="sub-loader position-relative ">
-            <div class="lds-hourglass"></div>
-            <p>Loading...</p>
+
+
+<div class="loader d-none">
+    <div class="sub-loader position-relative ">
+        <div class="lds-hourglass"></div>
+        <p>Loading...</p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <div class="alert alert-warning alert-block info-msg d-none">
+            <button type="button" class="close" data-dismiss="alert">×</button>
+            <strong id='info-value'></strong>
         </div>
     </div>
+    <div class="col-6">
 
-    <div class="row">
-        <div class="col"></div>
-        <div class="col-8">
+        @if (session()->has('success'))
+        <x-adminlte-alert theme="success" title="Success" dismissable>
+            {{ session()->get('success') }}
+        </x-adminlte-alert>
+        @endif
 
-            @if (session()->has('success'))
-                <x-adminlte-alert theme="success" title="Success" dismissable>
-                    {{ session()->get('success') }}
-                </x-adminlte-alert>
-            @endif
+        @if (session()->has('error'))
+        <x-adminlte-alert theme="danger" title="Error" dismissable>
+            {{ session()->get('error') }}
+        </x-adminlte-alert>
+        @endif
 
-            @if (session()->has('error'))
-                <x-adminlte-alert theme="danger" title="Error" dismissable>
-                    {{ session()->get('error') }}
-                </x-adminlte-alert>
-            @endif
+        <form class="row" action="{{ route('invoice.csv.file.import') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="col-3"></div>
+            <div class="col-6">
+                <x-adminlte-input label="Choose File" name="invoice_csv" id="asin" type="file" />
+            </div>
 
-            <form class="row" action="{{ route('invoice.csv.file.import') }}" method="POST"
-                enctype="multipart/form-data">
-                @csrf
-                <div class="col-3"></div>
-                <div class="col-6">
-                    <x-adminlte-input label="Choose File" name="invoice_csv" id="asin" type="file" />
+            <div class="col-3"></div>
+
+            <div class="col-12">
+                <div class="text-center">
+                    <x-adminlte-button label="Upload" theme="primary" icon="fas fa-plus" type="submit" id="invoice_upload_button" />
                 </div>
+            </div>
+        </form>
 
-                <div class="col-3"></div>
-
-                <div class="col-12">
-                    <div class="text-center">
-                        <x-adminlte-button label="Upload" theme="primary" icon="fas fa-plus" type="submit"
-                            id="invoice_upload_button" />
-                    </div>
-                </div>
-            </form>
-
-        </div>
-        <div class="col"></div>
     </div>
+    <div class="col"></div>
+
+</div>
 
 @stop
 
 @section('js')
-    <script>
-        $(document).ready(function() {
-            $.ajax({
-                method: 'get',
-                url: "/invoice/file/management/monitor/",
-                data: {
-                    "module_type": "IMPORT_INVOICE",
-                    "_token": "{{ csrf_token() }}",
-                },
-                response: 'json',
-                success: function(response) {
-                    // console.log(response);
-                    if (response == '0000-00-00 00:00:00') {
+<script>
+    $(document).ready(function() {
+        $.ajax({
+            method: 'get',
+            url: "/invoice/file/management/monitor/",
+            data: {
+                "module_type": "IMPORT_INVOICE",
+                "_token": "{{ csrf_token() }}",
+            },
+            response: 'json',
+            success: function(response) {
 
-                        $('#invoice_upload_button').prop('disabled', true);
-                        $('#invoice_upload_button').attr("title", "File is importing...");
-                    }
+                console.log(response);
+                if (response.status == 'Processing') {
+                    $('#invoice_upload_button').prop('disabled', true);
 
-                },
-            });
+                    $('.info-msg').removeClass('d-none');
+                    $('#info-value').html(response.description);
+
+                } else if (response.description) {
+
+                    $('.info-msg').removeClass('d-none');
+                    $('#info-value').html(response.description);
+                }
+            },
         });
+    });
 
-        $('#invoice_upload_button').click(function() {
-            $('#invoice_upload_button').html('<i class="fa fa-circle-o-notch fa-spin"></i> Uploading...');
+    $('#invoice_upload_button').click(function() {
+        $('#invoice_upload_button').html('<i class="fa fa-circle-o-notch fa-spin"></i> Uploading...');
 
-        });
-    </script>
+    });
+</script>
 @stop
