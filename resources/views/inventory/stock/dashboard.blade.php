67b9e4831b17896c745ccac898449611de9480ce
sanju <epsilon.palmate@gmail.com>
2022-12-06 12:26:32 +0530

    Pagination And Route fixed For Inventory Stock Module

--- resources/views/inventory/stock/dashboard.blade.php
+++ resources/views/inventory/stock/dashboard.blade.php
@@ -32,9 +32,10 @@
         <form class="row">
             <div class="col-2">
                 <x-adminlte-select name="ware_id" id="warehouse" label="Select Warehouse">
-                    <option value=" ">Select Warehouse</option>
+                    <option value="">Select Warehouse</option>
                     @foreach ($ware_lists as $ware_list)
-                    <option value="{{ $ware_list->warehouses->id }}">{{$ware_list->warehouses->name }}</option>
+                    <option value="{{ $ware_list->warehouses->id }}" {{$request_ware_id ==  $ware_list->warehouses->id  ? "selected" : ''}}>{{$ware_list->warehouses->name }}</option>
+
                     @endforeach
                 </x-adminlte-select>
             </div>
@@ -46,18 +47,9 @@
                     </div>
                 </h2>
             </div>
-            <h6 class="mb-4 text-right col">
-                <div class="search">
-                    <label>
-                        <div style="margin-top: 1.8rem;">
-                            Search:
-                            <input type="text" id="myInput" class="d-inline-block" placeholder="search asin" autocomplete="off" />
-                    </label>
-                </div>
+            </h6>
+        </form>
     </div>
-    </h6>
-    </form>
-</div>
 </div>
 @stop
 @section('content')
@@ -76,16 +68,17 @@
     </div>
 </div>
 
-<table class="table table-bordered yajra-datatable table-striped " id="detail_table">
+<table class="table table-bordered yajra-datatable table-striped" id="detail_table">
     <thead>
-        <tr>
+        <tr class="table-info">
+            <th>ID</th>
             <th>Warehouse Name</th>
             <th>Shipment ID</th>
             <th id='asin'>ASIN</th>
             <th>Item Name</th>
             <th>Price/Unit</th>
             <th>Quantity In.</th>
-            <th>Quantity/out.</th>
+            <th>Quantity out.</th>
             <th>Quantity Left</th>
             <th>Storage Shelve</th>
             <th>Inwarding Date</th>
@@ -99,84 +92,167 @@
 
 @section('js')
 <script type="text/javascript">
-    /*hide untill selection is done */
-
-    $("#detail_table,#export,.search").hide();
-    $("#warehouse").on('change', function(e) {
-        $("#detail_table,#export,.search").show();
-    });
-
-    /* Search function */
 
     $(document).ready(function() {
-        $("#myInput").on("keyup", function() {
-            var value = $(this).val().toLowerCase();
-            $("#data_display tr").filter(function() {
-                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-            });
-        });
+        if ($('#warehouse').val() == '') {
+            $('#export').hide();
+        }
     });
 
-    $('#warehouse').change(function(e) {
 
-        e.preventDefault();
-        var id = $(this).val();
+    $('#warehouse').on('change', function() {
+        window.location = "/inventory/stocks/list/" + $(this).val();
+    });
 
-        $.ajax({
-            method: 'GET',
-            url: '/inventory/list',
-            data: {
-                'id': id,
-                "_token": "{{ csrf_token() }}",
+
+    $.extend($.fn.dataTable.defaults, {
+        pageLength: 50,
+    });
+
+
+
+    let yajra_table = $('.yajra-datatable').DataTable({
+        processing: true,
+        serverSide: true,
+      
+        ajax: {
+            url: "{{ url($url) }}",
+            type: 'get',
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded',
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
             },
-            'dataType': 'json',
-            success: function(response) {
-                // console.log(response);
-
-                let html = '';
-                $.each(response, function(index, value) {
-
-                    const d = new Date(value.created_at);
-
-                    html += "<tr>";
-                    html += "<td>" + value.warehouses.name + "</td>";
-                    html += "<td>" + value.ship_id + "</td>";
-                    html += "<td>" + value.asin + "</td>";
-                    html += "<td>" + value.item_name + "</td>";
-                    html += "<td>" + value.price + "</td>";
-                    html += "<td>" + value.quantity + "</td>";
-
-                    if (value.out_quantity == null) {
-                        html += "<td>" + '0' + "</td>";
-                    } else {
-                        html += "<td>" + value.out_quantity + "</td>";
-
-                    }
-                    html += "<td>" + value.balance_quantity + "</td>";
-                    if (value.shelves == null) {
-                        html += "<td>" + 'Not Allocated' + "</td>"
-                    } else {
-                        html += "<td>" + value.shelves.rack_id + '-' + value.shelves.shelve_id + "</td>";
-                    }
-                    html += "<td>" + d.toDateString() + "</td>";
-                    html += "</tr>";
-
-                });
-                // html += "<td>" + value.bin + "</td>";
-
-                $("#data_display").html(html);
-
-                // window.location.href = '/inventory/exp/' + id;
-                // alert(' pdf Downloaded  successfully');
+            data: function(d) {
+                d.ware_id = $('#warehouse').val();
+            },
+        },
+        columns: [{
+                data: 'id',
+                name: 'id'
+            },
+            {
+                data: 'w_name',
+                name: 'w_name',
 
             },
-            error: function(response) {
-                // console.log(response);
-                alert('error');
+            {
+                data: 'ship_id',
+                name: 'ship_id',
+
+            },
+            {
+                data: 'asin',
+                name: 'asin',
+
+            },
+            {
+                data: 'item_name',
+                name: 'item_name'
+            },
+            {
+                data: 'price',
+                name: 'price',
+                orderable: false,
+                searchable: false
+            },
+            {
+                data: 'quantity',
+                name: 'quantity',
+                orderable: false,
+                searchable: false
+            },
+            {
+                data: 'out_quantity',
+                name: 'out_quantity',
+                orderable: false,
+                searchable: false
+            },
+            {
+                data: 'balance_quantity',
+                name: 'balance_quantity',
+                orderable: false,
+                searchable: false
+            },
+            {
+                data: 'bin',
+                name: 'bin',
+            },
+            {
+                data: 'created_at',
+                name: 'created_at',
+                orderable: false,
+                searchable: false
+
             }
-        });
-
+        ]
     });
+
+
+
+    // $('#warehouse').change(function(e) {
+
+    //     e.preventDefault();
+    //     var id = $(this).val();
+
+    //     $.ajax({
+    //         method: 'GET',
+    //         url: '/inventory/list',
+    //         data: {
+    //             'id': id,
+    //             "_token": "{{ csrf_token() }}",
+    //         },
+    //         'dataType': 'json',
+    //         success: function(response) {
+    //             // console.log(response);
+
+    //             let html = '';
+    //             $.each(response, function(index, value) {
+
+    //                 const d = new Date(value.created_at);
+
+    //                 html += "<tr>";
+    //                 html += "<td>" + value.warehouses.name + "</td>";
+    //                 html += "<td>" + value.ship_id + "</td>";
+    //                 html += "<td>" + value.asin + "</td>";
+    //                 html += "<td>" + value.item_name + "</td>";
+    //                 html += "<td>" + value.price + "</td>";
+    //                 html += "<td>" + value.quantity + "</td>";
+
+    //                 if (value.out_quantity == null) {
+    //                     html += "<td>" + '0' + "</td>";
+    //                 } else {
+    //                     html += "<td>" + value.out_quantity + "</td>";
+
+    //                 }
+    //                 html += "<td>" + value.balance_quantity + "</td>";
+    //                 if (value.shelves == null) {
+    //                     html += "<td>" + 'Not Allocated' + "</td>"
+    //                 } else {
+    //                     html += "<td>" + value.shelves.rack_id + '-' + value.shelves.shelve_id + "</td>";
+    //                 }
+    //                 html += "<td>" + d.toDateString() + "</td>";
+    //                 html += "</tr>";
+
+    //             });
+    //             // html += "<td>" + value.bin + "</td>";
+
+    //             $("#data_display").html(html);
+
+    //             // window.location.href = '/inventory/exp/' + id;
+    //             // alert(' pdf Downloaded  successfully');
+
+    //         },
+    //         error: function(response) {
+    //             // console.log(response);
+    //             alert('error');
+    //         }
+    //     });
+
+    // });
+
+
+
+
 
     /*download PDF */
     $('#export').click(function() {
@@ -194,7 +270,6 @@
             success: function(result) {
                 window.location.href = '/inventory/exp/' + id;
                 alert(' Downloaded successfully');
-
 
             },
             error: function(response) {
