d5b5e946915f0df59a8aea2bd5a37e15d9e2b42a
amitpms <nu.palmate@gmail.com>
2022-12-06 11:15:14 +0530

    catalog import every two minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -39,7 +39,7 @@ class Kernel extends ConsoleKernel
             $schedule->command('pms:remove-uploaded-boe')->dailyAt('01:00'); //PM
 
             /*Catalog*/
-            $schedule->command('mosh:catalog-amazon-import')->everyMinute(); //PM
+            $schedule->command('mosh:catalog-amazon-import')->everyTwoMinutes(); //PM
             $schedule->command('mosh:Catalog-price-import-bb-in')->everyMinute(); //PM
             $schedule->command('mosh:Catalog-price-import-bb-us')->everyMinute(); //PM
             $schedule->command('mosh:catalog-dashboard-file')->hourly(); //PM
