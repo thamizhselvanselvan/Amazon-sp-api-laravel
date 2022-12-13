2aee897d345819f4431f877f41b27acc631c83a3
sanju <epsilon.palmate@gmail.com>
2022-12-13 18:29:11 +0530

    @ banner Section Added

--- config/adminlte.php
+++ config/adminlte.php
@@ -938,17 +938,24 @@ return [
                     'icon' => 'fa fa-home',
                     'submenu' => [
                         [
-                            'text' => '3 Banner Section',
-                            'url' => 'cliqnshop/banner',
-                            'can' => ['Admin', 'Cliqnshop'],
-                            'icon' => 'fa fa-image',
-                        ],
-                        [
                             'text' => 'Top Selling Section',
                             'url' => 'cliqnshop/brand',
                             'can' => ['Admin', 'Cliqnshop'],
                             'icon' => 'fa fa-arrow-up',
                         ],
+                        [
+                            'text' => '2 Banner Section',
+                            'url' => 'cliqnshop/two_banners',
+                            'can' => ['Admin', 'Cliqnshop'],
+                            'icon' => 'fa fa-image',
+                        ],
+                        [
+                            'text' => '3 Banner Section',
+                            'url' => 'cliqnshop/banner',
+                            'can' => ['Admin', 'Cliqnshop'],
+                            'icon' => 'fa fa-image',
+                        ],
+                        
                     ],
                 ],
             ],
