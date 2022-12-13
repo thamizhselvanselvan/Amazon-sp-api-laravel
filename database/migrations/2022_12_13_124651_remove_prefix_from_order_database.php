0d703a345ecd8b954a0b2931c29a84dec480aff2
amitpms <nu.palmate@gmail.com>
2022-12-13 16:23:27 +0530

    prefix removed from order table

---
+++ database/migrations/2022_12_13_124651_remove_prefix_from_order_database.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RemovePrefixFromOrderDatabase extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('order')->rename('ord_order_seller_credentials', 'order_seller_credentials');
+        Schema::connection('order')->rename('ord_order_update_details', 'order_update_details');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('order')->rename('order_seller_credentials', 'ord_order_seller_credentials');
+        Schema::connection('order')->rename('order_update_details', 'ord_order_update_details');
+    }
+}
