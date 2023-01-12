9014b49f12d2bacd81c19fc63c4c29974fb30653
amitpms <nu.palmate@gmail.com>
2023-01-12 11:19:42 +0530

    label inward tracking id added

--- routes/pms/label.php
+++ routes/pms/label.php
@@ -31,7 +31,7 @@ Route::post('label/missing/address', 'label\labelManagementController@labelMissi
 Route::get('label/missing/address/export', 'label\labelManagementController@labelMissingAddressExport')->name('label.missing.address.export');
 
 Route::match(['get', 'post'], 'label/search/amazon-order-id', 'label\labelManagementController@labelSearchByOrderId')->name('lable.search.amazon-order-id');
-Route::match(['get', 'post'], 'label/search/awb-tracking-id', 'label\labelManagementController@labelSearchByAwnNo')->name('lable.search.by.awb.no');
+// Route::match(['get', 'post'], 'label/search', 'label\labelManagementController@labelSearchAll')->name('lable.search.all');
 Route::match(['get', 'post'], 'label/update/tracking-details', 'label\labelManagementController@updateTrackingDetails')->name('lable.update.tracking-details');
 Route::get('label/edit-order-address/{id}', 'label\labelManagementController@editOrderAddress');
 Route::put('label/update-order-address/{id}', 'label\labelManagementController@updateOrderAddress');
