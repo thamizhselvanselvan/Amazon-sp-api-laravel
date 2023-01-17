1aa3a2c643feb1f4bacc0ddd8c1e49efe83a4c74
satish singh devbha <kappa.palmate@gmail.com>
2023-01-17 18:44:37 +0530

    Product Feed APi added & Product Price Fetch & Price push

--- app/Http/Controllers/Buybox_stores/BuyBoxStoreController.php
+++ app/Http/Controllers/Buybox_stores/BuyBoxStoreController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Buybox_stores;
 
 use Illuminate\Http\Request;
 use App\Models\Aws_credential;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 use App\Models\Buybox_stores\Product;
 use Illuminate\Support\Facades\Storage;
+use Yajra\DataTables\Facades\DataTables;
 
 class BuyBoxStoreController extends Controller
 {
@@ -49,6 +51,7 @@ class BuyBoxStoreController extends Controller
         commandExecFunc("mosh:export_all_stores_asins");
         return redirect()->route('buybox.stores')->with('success', 'Export Started Please Wait For Some time...');
     }
+
     public function exportdownload(Request $request)
     {
         $catalogfiles = [];
@@ -67,5 +70,26 @@ class BuyBoxStoreController extends Controller
     public function DownloadCataloglocal($index)
     {
         return Storage::download('aws-products/exports/' . $index);
+    }
+
+    public function get_price_push(Request $request) {
+        
+        if ($request->ajax()) {
+
+            $results = DB::connection("buybox_stores")->table("product_push")
+            ->select('id', 'store_id', 'product_sku', 'latency', 'push_price', 'base_price')
+            ->orderBy('id', 'DESC')
+            ->get();
+
+            return DataTables::of($results)
+                // ->addColumn('status', function ($file_management) {
+                //     $process = $file_management['command_end_time'] == '0000-00-00 00:00:00' ? 'Processing...' : 'Processed';
+                //     return $process;
+                // })
+               // ->rawColumns(['id', 'user_name', 'type', 'module', 'start_time', 'end_time', 'processed_time', 'status'])
+                ->make(true);
+        }
+        
+        return view('buybox_stores.sp_api_push');
     }
 }
--- routes/pms/buybox_stores.php
+++ routes/pms/buybox_stores.php
@@ -7,3 +7,6 @@ Route::post('buybox/latency', 'Buybox_stores\BuyBoxStoreController@latencyupdate
 Route::get('buybox/all/export', 'Buybox_stores\BuyBoxStoreController@exportall')->name('buybox.export.all');
 Route::get('buybox/all/export/download', 'Buybox_stores\BuyBoxStoreController@exportdownload')->name('buybox.export.all.download');
 Route::get('buybox/all/export/download/local/{index}', 'Buybox_stores\BuyBoxStoreController@DownloadCataloglocal')->name('buybox.download.all.lacal');
+
+
+Route::get('buybox/sp_api_push', 'Buybox_stores\BuyBoxStoreController@get_price_push')->name('buybox.sp_spi_push_get');
