0585cf993e2dc173a4b5f1b87d6a43beac42103b
sanju <epsilon.palmate@gmail.com>
2023-02-08 14:35:25 +0530

    Cliqnshop short description remove

--- routes/v2/test-sanjay.php
+++ routes/v2/test-sanjay.php
@@ -5,5 +5,5 @@ use App\Services\Zoho\ZohoApi;
 use Illuminate\Support\Facades\Route;
 use App\Models\order\OrderItemDetails;
 
-Route::get('mosh/test/order/{order_id}/{seller_id}/{country_code}', 'TestController@getOrder');
+Route::get('test/order/{order_id}/{seller_id}/{country_code}', 'TestController@getOrder');
 
