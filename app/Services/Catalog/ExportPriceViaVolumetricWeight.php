d633bebbff92a261d2e3fe2c049f84f6724881e4
vikesh <zeta.palmate@gmail.com>
2023-02-08 20:46:06 +0530

    Fixed.

--- app/Services/Catalog/ExportPriceViaVolumetricWeight.php
+++ app/Services/Catalog/ExportPriceViaVolumetricWeight.php
@@ -162,9 +162,9 @@ class ExportPriceViaVolumetricWeight
             $length = 0;
             $width = 0;
             $packet_dimensions = 0;
-            $dimension = json_decode($catalog_detail['dimensions'], true);
+            $dimension = (array) json_decode($catalog_detail['dimensions'], true);
 
-            if (array_key_exists('package', $dimension[0])) {
+            if (array_key_exists('package', $dimension[0]) && gettype($dimension[0]) == 'array') {
                 if (isset($dimension[0]['package']['weight']['value']) || isset($dimension[0]['package']['height']['value']) || isset($dimension[0]['package']['length']['value']) || isset($dimension[0]['package']['width']['value'])) {
 
                     $weight = $dimension[0]['package']['weight']['value'] ?? 0;
