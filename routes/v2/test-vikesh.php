a5e348e1d0b2cdaaedf7b63101b3f2194d74ce7d
vikesh <zeta.palmate@gmail.com>
2023-02-15 13:24:47 +0530

    BuyBox auto export completed.

--- routes/v2/test-vikesh.php
+++ routes/v2/test-vikesh.php
@@ -1,6 +1,8 @@
 <?php
 
+use Carbon\Carbon;
 use League\Csv\Writer;
+use App\Models\FileManagement;
 use App\Models\Catalog\catalogae;
 use App\Models\Catalog\catalogin;
 use App\Models\Catalog\catalogsa;
@@ -46,5 +48,50 @@ Route::get('export', function () {
         $table = table_model_create(country_code: 'in', model: 'Asin_source', table_name: 'asin_source_');
         $table->upsert($asin, ['user_asin_unique'], ['asin', 'status']);
         Log::warning('successfully' . $i);
+    }
+});
+
+Route::get('test', function () {
+    $today = Carbon::today()->toDateTimeString();
+    $now = Carbon::now()->toDateTimeString();
+    po($today);
+    // exit;
+    // $data = FileManagement::where("module", "BUYBOX_EXPORT_IN_1")
+    //     ->whereBetween('created_at', [$today, '2023-02-15 12:01:53'])
+    //     ->count();
+
+    // po($data);
+    // exit;
+
+    $user_id = 124;
+    $priority = 1;
+    $countryCode = ['1' => 'IN', '2' => 'US',  '3' => 'AE'];
+    $key = 1;
+    $file_info = [];
+    // foreach ($countryCode as $country) {
+    Query:
+    // if ($key < 4) {
+
+    $data = FileManagement::where("module", "BUYBOX_EXPORT_$countryCode[$key]_1")
+        ->whereBetween('created_at', [$today, $now])
+        ->count();
+    if ($data == '0') {
+
+        $file_info = [
+            "user_id"        => $user_id,
+            "type"           => "BUYBOX_EXPORT",
+            "module"         => "BUYBOX_EXPORT_$countryCode[$key]_1",
+            "command_name"   => "mosh:buybox-export-asin"
+        ];
+        FileManagement::create($file_info);
+    } else {
+
+        $key++;
+
+        if ($key > 3) {
+            exit;
+        }
+
+        goto Query;
     }
 });
