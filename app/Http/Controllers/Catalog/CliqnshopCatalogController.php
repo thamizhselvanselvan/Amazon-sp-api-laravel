f321fba148f0885fc4cd9538860c41225e27dcef
vikesh <zeta.palmate@gmail.com>
2023-02-16 17:12:17 +0530

    Fixed catalog export for cliqnshop.

--- app/Http/Controllers/Catalog/CliqnshopCatalogController.php
+++ app/Http/Controllers/Catalog/CliqnshopCatalogController.php
@@ -80,19 +80,31 @@ class CliqnshopCatalogController extends Controller
         if (!Storage::exists($path)) {
             return false;
         } else {
-            // $user_id = Auth::user()->id;
-            // $header = ["path" => "${path}" , "site_id"=>"${site_id}"];
+            $user_id = Auth::user()->id;
+            // $header = ["path" => "${path}", "site_id" => "${site_id}"];
+            $file_info = [
+                'user_id' => $user_id,
+                'type' => 'Import',
+                'module' => "Cliqnshop_insert",
+                'file_path' => $path,
+                'command_name' => "mosh:catalog_insert_cliqnshop",
+                'command_start_time' => now(),
+                'command_end_time' => now(),
+            ];
+
+            FileManagement::create($file_info);
+            // fileManagement();
+
             // $file_info = [
             //     'user_id' => $user_id,
             //     'type' => 'Import',
-            //     'module' => "Cliqnshop_insert",
+            //     'module' => "Cliqnshop_export",
             //     'file_path' => $path,
-            //     'command_name' => "mosh:catalog_insert_cliqnshop",
-            //     "header"        => "${path}_${site_id}"
+            //     'command_name' => "mosh:export_catalog_imported_asin ${path}",
             // ];
 
             // FileManagement::create($file_info);
-          
+            // fileManagement();
 
 
             commandExecFunc("mosh:catalog_insert_cliqnshop ${path} ${site_id}");
