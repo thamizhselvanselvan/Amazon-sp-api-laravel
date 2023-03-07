1f7292a6515005ff4428d80affd57867a7bab7a2
sanju <epsilon.palmate@gmail.com>
2023-03-07 19:30:09 +0530

    Backup Module done

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Admin\Backup;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,6 +27,20 @@ class AppServiceProvider extends ServiceProvider
     {
         if (app()->environment() === 'production') {
             //DB::statement('SET SESSION sql_require_primary_key=0');
+
+        }
+
+        $datas =  Backup::where("status", 1)->get(["connection", "table_name"])->groupBy("connection");
+
+        foreach ($datas as $connection => $table_names) {
+
+            $table_names = collect($table_names)->pluck("table_name");
+
+            Config::set(
+                "database.connections.{$connection}.dump.excludeTables",
+                $table_names
+            );
+        
         }
     }
 }
