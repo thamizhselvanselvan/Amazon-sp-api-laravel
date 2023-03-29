a10b35bb840cc8db52381ae635a24c06f197b69c
sanju <epsilon.palmate@gmail.com>
2023-03-29 12:02:10 +0530

    SNT Courier Status Management Data Insert

--- app/Console/Commands/Shipntrack/Courier/CourierStatusfetchandInsert.php
+++ app/Console/Commands/Shipntrack/Courier/CourierStatusfetchandInsert.php
@@ -49,17 +49,18 @@ class CourierStatusfetchandInsert extends Command
             ->get();
 
         $courier_code_sm =   Courier::query()->where('courier_name', 'SMSA')->select('id')->get();
-        if (isset($courier_code_sm[0]->id)) {
+        $code_sm = '';
+        if (isset($courier_code_sm[0]->id) && (count($smsa_data) > 0)) {
             $code_sm = $courier_code_sm[0]->id;
-        }
-        foreach ($smsa_data as $datas) {
-            $data = [
-                'courier_id' => $code_sm,
-                'courier_status' => $datas->activity
-            ];
-            StatusManagement::upsert($data, ['cp_status_cp_id_unique'], ['courier_id', 'courier_status']);
-        }
 
+            foreach ($smsa_data as $datas) {
+                $data = [
+                    'courier_id' => $code_sm,
+                    'courier_status' => $datas->activity
+                ];
+                StatusManagement::upsert($data, ['cp_status_cp_id_unique'], ['courier_id', 'courier_status']);
+            }
+        }
 
 
         // Bombino Status Fetch and Insert Into Status Master
@@ -69,19 +70,19 @@ class CourierStatusfetchandInsert extends Command
             ->get();
 
         $courier_code_bom =   Courier::query()->where('courier_name', 'Bombino')->select('id')->get();
-        if (isset($courier_code_bom[0]->id)) {
+        $code_bom = '';
+        if (isset($courier_code_bom[0]->id) && (count($bombono_data) > 0)) {
 
             $code_bom = $courier_code_bom[0]->id;
-        }
-        foreach ($bombono_data as $data_bom) {
-            $data = [
-                'courier_id' => $code_bom,
-                'courier_status' => $data_bom->event_code
-            ];
-            StatusManagement::upsert($data, ['cp_status_cp_id_unique'], ['courier_id', 'courier_status']);
-        }
 
-
+            foreach ($bombono_data as $data_bom) {
+                $data = [
+                    'courier_id' => $code_bom,
+                    'courier_status' => $data_bom->event_code
+                ];
+                StatusManagement::upsert($data, ['cp_status_cp_id_unique'], ['courier_id', 'courier_status']);
+            }
+        }
 
         // Aramax Status Fetch and Insert Into Status Master
         $aramax_data = AramexTracking::query()
@@ -90,15 +91,17 @@ class CourierStatusfetchandInsert extends Command
             ->get();
 
         $courier_code_aramax =   Courier::query()->where('courier_name', 'Aramex')->select('id')->get();
-        if (isset($courier_code_aramax[0]->id)) {
+        $code_aramax = '';
+        if (isset($courier_code_aramax[0]->id) && count($aramax_data) > 0) {
             $code_aramax = $courier_code_aramax[0]->id;
-        }
-        foreach ($aramax_data as $data_bom) {
-            $data = [
-                'courier_id' => $code_aramax,
-                'courier_status' => $data_bom->update_description
-            ];
-            StatusManagement::upsert($data, ['cp_status_cp_id_unique'], ['courier_id', 'courier_status']);
+
+            foreach ($aramax_data as $data_bom) {
+                $data = [
+                    'courier_id' => $code_aramax,
+                    'courier_status' => $data_bom->update_description
+                ];
+                StatusManagement::upsert($data, ['cp_status_cp_id_unique'], ['courier_id', 'courier_status']);
+            }
         }
     }
 }
