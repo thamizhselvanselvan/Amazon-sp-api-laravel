5c3f284139b5db5bcbab945563de5b8062453c48
Thamizhselvan Selvan <thamizhselvanselvan707@gmail.com>
2023-06-08 19:52:09 +0530

    changed shipntrack operation icon.

--- app/Http/Controllers/shipntrack/Manifest/ExportManifestController.php
+++ app/Http/Controllers/shipntrack/Manifest/ExportManifestController.php
@@ -24,7 +24,7 @@ class ExportManifestController extends Controller
             return DataTables::of($data)
                 ->addIndexColumn()
                 ->addColumn('action', function ($row) {
-                    return '<div class="d-flex"><a href="/shipntrack/export/' . $row->id . '/view" class="edit btn btn-success btn-sm"><i class="fas fa-edit"></i> View</a>';
+                    return '<div class="d-flex"><a href="/shipntrack/export/' . $row->manifest_id . '/view" class="edit btn btn-success btn-sm"><i class="fas fa-edit"></i> View</a>';
                 })
                 ->rawColumns(['action'])
                 ->make(true);
